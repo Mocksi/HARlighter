a0be64c03fa0e700f2ac330da92153856f169429
Nicolas Moreno <nicolasmoreno@sirius.com.ar>
2024-05-06 15:17:49 -0300

    opening mocksi.ai login page after installing the extension

--- apps/mocksi-lite/background.ts
+++ apps/mocksi-lite/background.ts
@@ -1,1 +1,9 @@
 console.log('Hello from the background script!')
+
+addEventListener("install", () => {
+  // TODO test if this works on other browsers
+  // TODO2 Read from environment variable the correct URL to redirect after install
+  chrome.tabs.create({
+    url: "https://mocksi.ai/login"
+  })
+})
