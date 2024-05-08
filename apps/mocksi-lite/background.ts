d60051465f1423861ef11f5aaa1426cd0b0b32bf
Nicolas Moreno <nicolasmoreno@sirius.com.ar>
2024-05-08 17:42:02 -0300

    reverted login url after installing

--- apps/mocksi-lite/background.ts
+++ apps/mocksi-lite/background.ts
@@ -4,7 +4,6 @@ addEventListener("install", () => {
   // TODO test if this works on other browsers
   // TODO2 Read from environment variable the correct URL to redirect after install
   chrome.tabs.create({
-    url: "http://localhost:3000"
-    // url: "https://mocksi.ai/login"
+    url: "https://mocksi.ai/login"
   })
 })
