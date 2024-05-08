615b63c3cbc141fee0e9ea98b954fd6378c64831
Nicolas Moreno <nicolasmoreno@sirius.com.ar>
2024-05-08 17:34:46 -0300

    Working on ui packages components

--- packages/ui/src/input.tsx
+++ packages/ui/src/input.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ChangeEventHandler } from "react";
+import { ChangeEventHandler, useState } from "react";
 
 interface InputProps {
   value?: string;
@@ -10,12 +10,23 @@ interface InputProps {
   onChange?: ChangeEventHandler
 }
 
-
 export const Input = ({ value, inputLabel, labelClassName ,className, onChange }: InputProps) => {
+  const [focused, setFocused] = useState(false)
   return (
     <>
-      <label className={labelClassName} style={{ display: 'flex', flexDirection: 'column' }}>{inputLabel}
+      <label className={labelClassName} style={{ display: 'flex', flexDirection: 'column', gap: '8px' }}>
+        {inputLabel}
         <input
+          style={{
+            outline: 'none',
+            borderRadius: '8px',
+            borderWidth: '2px',
+            borderColor: focused ? '#009875' : '#ABABAB',
+            height: '40px',
+            boxShadow: '0px 0px 8px 0px #0000001A'
+          }}
+          onFocus={() => setFocused(true)}
+          onBlur={() => setFocused(false)}
           className={className}
           value={value}
           onChange={onChange}
