71039f84131af0d74d326bf0da26f4a0205e1498
Nicolas Moreno <nicolasmoreno@sirius.com.ar>
2024-05-22 10:38:27 -0300

    fixed images issue

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   output: 'export',
  
+  images: { unoptimized: true },
   // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
   // trailingSlash: true,
  
