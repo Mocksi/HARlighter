c39c6c40238442cc69e323bfc91c830cc13e086d
Nicolas Moreno <67698200+NicoMorenoSirius@users.noreply.github.com>
2024-05-22 12:51:56 -0300

    Deploy frontend dev config (#10)
    * implemented hooks and functions to make api calls
    
    * added auth url
    
    * added success login component
    
    * added clearing error message after 7 seconds
    
    * added clearing error message after 7 seconds
    
    * some minor changes
    
    * wip loading status
    
    * extension popup loading(analyzing) state
    
    * fixed popup label
    
    * reverted login url after installing
    
    * set to false initial value of submittedCode
    
    * finishing integrating with auth0 backend
    
    * finished integrating with backend login
    
    * biome format
    
    * biome format
    
    * biome check fix
    
    * biome check fix
    
    * Integrated extension with login credentials from backend
    
    * Restricted extension to record if user is not logged in
    
    * run biome format
    
    * changed login and cookie url
    
    * added todo comment
    
    * changed nextjs to static site
    
    * fixed images issue
    
    * changed login url
    
    * added favicon

--- apps/mocksi-lite/background.ts
+++ apps/mocksi-lite/background.ts
@@ -2,7 +2,7 @@ addEventListener("install", () => {
 	// TODO test if this works on other browsers
 	// TODO2 Read from environment variable the correct URL to redirect after install
 	chrome.tabs.create({
-		url: "https://mocksi.ai/login",
+		url: "https://mocksi-auth.onrender.com/",
 	});
 });
 
@@ -10,7 +10,7 @@ addEventListener("install", () => {
 // TODO2 What should we do if user is not logged in?
 chrome.action.onClicked.addListener((tab) => {
 	chrome.cookies.get(
-		{ url: "https://mocksi.ai/", name: "sessionid" },
+		{ url: "https://mocksi-auth.onrender.com/", name: "sessionid" },
 		(cookie) => {
 			chrome.tabs.sendMessage(tab?.id || 0, {
 				text: "clickedIcon",
--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
+const nextConfig = {
+  output: 'export',
+ 
+  images: { unoptimized: true },
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+ 
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+ 
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
+
   transpilePackages: ["@repo/ui"],
-};
+}
+module.exports = nextConfig
