fb7095ce7729bc2bf9b4c9fbc783a6cdb2609573
Nicolas Moreno <nicolasmoreno@sirius.com.ar>
2024-05-24 13:25:57 -0300

    biome fixes

--- apps/mocksi-lite/content/content.tsx
+++ apps/mocksi-lite/content/content.tsx
@@ -18,60 +18,75 @@ chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
 	}
 });
 
-
 function decorate(text: string) {
-	// TODO! Detect if parent node was a plain text or a h1,h2,h3,h4,etc. To keep the previous element 
+	// TODO! Detect if parent node was a plain text or a h1,h2,h3,h4,etc. To keep the previous element
 	const newSpan = document.createElement("span");
-    newSpan.style.border = "3px orange solid";
-	newSpan.id = 'mocksiSelectedText'
-    newSpan.appendChild(document.createTextNode(text));
-    return newSpan;
+	newSpan.style.border = "3px orange solid";
+	newSpan.id = "mocksiSelectedText";
+	newSpan.appendChild(document.createTextNode(text));
+	return newSpan;
 }
 
-function decorateTextTag(text: string, {startOffset, endOffset}: {startOffset: number, endOffset: number}) {
+function decorateTextTag(
+	text: string,
+	{ startOffset, endOffset }: { startOffset: number; endOffset: number },
+) {
 	const fragment = document.createDocumentFragment();
-	if(startOffset > 0) fragment.appendChild(document.createTextNode(text.substring(0, startOffset)));
+	if (startOffset > 0)
+		fragment.appendChild(
+			document.createTextNode(text.substring(0, startOffset)),
+		);
 	fragment.appendChild(decorate(text.substring(startOffset, endOffset)));
-	if(endOffset < text.length) fragment.appendChild(document.createTextNode(text.substring(endOffset, text.length)));
-	return fragment
+	if (endOffset < text.length)
+		fragment.appendChild(
+			document.createTextNode(text.substring(endOffset, text.length)),
+		);
+	return fragment;
 }
 
-
-function applyHighlight2(targetedElement: HTMLElement, selectedRange: any) {
-	targetedElement.childNodes.forEach((node) => {
+function applyHighlight2(
+	targetedElement: HTMLElement,
+	selectedRange: Selection | null,
+) {
+	if (selectedRange === null) return;
+	for (const node of targetedElement.childNodes) {
 		if (node === selectedRange.anchorNode) {
 			//@ts-ignore
 			if (node.innerHTML) {
 				// I don't know which case can enter here, made it just in case. Probably this get removed
 				//@ts-ignore
-				node.innerHTML = decorateTextTag(selectedRange.anchorNode.textContent, selectedRange.getRangeAt(0))
-			}
-			else {
+				node.innerHTML = decorateTextTag(
+					selectedRange.anchorNode?.textContent || "",
+					selectedRange.getRangeAt(0),
+				);
+			} else {
 				// @ts-ignore
 				targetedElement.replaceChild(
-					decorateTextTag(selectedRange.anchorNode.textContent, selectedRange.getRangeAt(0)), // new node
-					node
-				)
+					decorateTextTag(
+						selectedRange.anchorNode?.textContent || "",
+						selectedRange.getRangeAt(0),
+					), // new node
+					node,
+				);
 			}
-			
 		}
-	})
+	}
 }
 
 function onDoubleClickText(event: MouseEvent) {
-	const previousSelectedText = document.getElementById('mocksiSelectedText')
-	const targetedElement: HTMLElement = event.target as HTMLElement
+	const previousSelectedText = document.getElementById("mocksiSelectedText");
+	const targetedElement: HTMLElement = event.target as HTMLElement;
 	// TODO if user selected a space text, break
 	// TODO2 if user the previously selected text, break
 	if (previousSelectedText) {
-		previousSelectedText.style.border = ''
-		previousSelectedText.id = '' // see if there's a better way to do this
+		previousSelectedText.style.border = "";
+		previousSelectedText.id = ""; // see if there's a better way to do this
 	}
-	const {startOffset, endOffset} = window.getSelection()?.getRangeAt(0) || {}
+	const { startOffset, endOffset } = window.getSelection()?.getRangeAt(0) || {};
 	if (startOffset !== undefined && endOffset !== undefined) {
-		applyHighlight2(targetedElement, window.getSelection())
+		applyHighlight2(targetedElement, window.getSelection());
 	} else {
-		console.log('ERROR! no offset detected', targetedElement)
+		console.log("ERROR! no offset detected", targetedElement);
 	}
 }
 
