a562fd8b28401df8744e5740c4704f5e633373de
Nicolas Moreno <67698200+NicoMorenoSirius@users.noreply.github.com>
2024-06-03 17:48:11 -0300

    Text replacer in DOM V1 (#24)
    * first version of selecting DOM text
    
    * biome fixes
    
    * biome fixes
    
    * changed name function
    
    * made v2 of DOM picker
    
    * biome fixes
    
    * wip injecting textarea to selected dom text
    
    * wip progress on changing DOM texts
    
    * wip progress on changing DOM texts
    
    * wip progress on changing DOM texts
    
    * feature: DOM text replacer v1
    
    * run biome format
    
    * now adding editor mode and removing it when user pleases
    
    * run biome format

--- apps/mocksi-lite/content/Popup/CreateDemo/DemoItem.tsx
+++ apps/mocksi-lite/content/Popup/CreateDemo/DemoItem.tsx
@@ -2,6 +2,7 @@ import Button, { Variant } from "../../../common/Button";
 import TextField from "../../../common/TextField";
 import editIcon from "../../../public/edit-icon.png";
 import exportIcon from "../../../public/export-icon.png";
+import { setEditorMode } from "../../content";
 import type { Demo } from "./index";
 
 const DemoItem = ({ name, customer }: Demo) => {
@@ -12,10 +13,10 @@ const DemoItem = ({ name, customer }: Demo) => {
 				<TextField>{customer}</TextField>
 			</div>
 			<div className={"flex gap-3"}>
-				<Button variant={Variant.icon} onClick={() => {}}>
+				<Button variant={Variant.icon} onClick={() => setEditorMode(true)}>
 					<img src={editIcon} alt={"editIcon"} />
 				</Button>
-				<Button variant={Variant.icon} onClick={() => {}}>
+				<Button variant={Variant.icon} onClick={() => setEditorMode(false)}>
 					<img src={exportIcon} alt={"exportIcon"} />
 				</Button>
 			</div>
--- apps/mocksi-lite/content/content.tsx
+++ apps/mocksi-lite/content/content.tsx
@@ -3,6 +3,170 @@ import ContentApp from "./ContentApp";
 
 let root: ReactDOM.Root;
 
+function decorate(text: string, width: string, shiftMode: boolean) {
+	const newSpan = document.createElement("span");
+	newSpan.style.position = "relative";
+	newSpan.id = "mocksiSelectedText";
+	newSpan.appendChild(document.createTextNode(text));
+	const textArea = elementWithBorder(
+		"textarea",
+		shiftMode ? width : undefined,
+		text,
+	);
+	newSpan.appendChild(textArea);
+	return newSpan;
+}
+
+function decorateTextTag(
+	text: string,
+	width: string,
+	shiftMode: boolean,
+	{ startOffset, endOffset }: { startOffset: number; endOffset: number },
+) {
+	const fragment = document.createDocumentFragment();
+	if (startOffset > 0)
+		fragment.appendChild(
+			document.createTextNode(text.substring(0, startOffset)),
+		);
+	fragment.appendChild(
+		decorate(text.substring(startOffset, endOffset), width, shiftMode),
+	);
+	if (endOffset < text.length)
+		fragment.appendChild(
+			document.createTextNode(text.substring(endOffset, text.length)),
+		);
+	return fragment;
+}
+
+function applyEditor(
+	targetedElement: HTMLElement,
+	selectedRange: Selection | null,
+	shiftMode: boolean,
+) {
+	if (selectedRange === null || selectedRange.anchorNode === null) return;
+	// this case is if the beggining node is the same as the finished one.
+	// this can happen while selecting text, there are more than one different node involved.
+	if (selectedRange.anchorNode === selectedRange.focusNode) {
+		for (const node of targetedElement.childNodes) {
+			if (
+				node === selectedRange.anchorNode ||
+				[...node.childNodes].includes(selectedRange.anchorNode as ChildNode)
+			) {
+				//@ts-ignore
+				if (node.innerHTML) {
+					// I don't know which case can enter here, made it just in case. Probably this get removed
+					//@ts-ignore
+					node.innerHTML = decorateTextTag(
+						selectedRange.anchorNode?.textContent || "",
+						targetedElement.clientWidth?.toString() || "",
+						shiftMode,
+						selectedRange.getRangeAt(0),
+					);
+				} else {
+					// @ts-ignore
+					targetedElement.replaceChild(
+						decorateTextTag(
+							selectedRange.anchorNode?.textContent || "",
+							targetedElement.clientWidth?.toString() || "",
+							shiftMode,
+							selectedRange.getRangeAt(0),
+						), // new node
+						node,
+					);
+				}
+			}
+		}
+	} else {
+		// TODO
+	}
+}
+
+function elementWithBorder(
+	elementType: string,
+	width: string | undefined,
+	value: string,
+) {
+	const ndiv = document.createElement(elementType || "div");
+	ndiv.setAttribute("tabindex", "-1");
+	const elementStyle = {
+		width: width ? "120%" : "150%",
+		height: "100%",
+		border: "1px solid red",
+		position: "absolute",
+		top: "0",
+		left: "0",
+		zIndex: "999",
+		background: "#f0f8ffa8",
+	};
+	ndiv.style.width = elementStyle.width;
+	ndiv.style.height = elementStyle.height;
+	ndiv.style.border = elementStyle.border;
+	ndiv.style.position = elementStyle.position;
+	ndiv.style.top = elementStyle.top;
+	ndiv.style.left = elementStyle.left;
+	ndiv.style.zIndex = elementStyle.zIndex;
+	ndiv.style.background = elementStyle.background;
+	ndiv.onkeydown = (event: KeyboardEvent) => {
+		if (event.key === "Enter" && !event.shiftKey) {
+			if (!event.repeat) {
+				const newEvent = new Event("submit", { cancelable: true });
+				event.target?.dispatchEvent(newEvent);
+			}
+			event.preventDefault(); // Prevents the addition of a new line in the text field
+		} else if (event.key === "Escape") {
+			const selectedText = document.getElementById("mocksiSelectedText");
+			const parentElement = selectedText?.parentElement;
+			selectedText?.parentElement?.replaceChild(
+				document.createTextNode(value),
+				selectedText,
+			);
+			parentElement?.normalize();
+		}
+	};
+	ndiv.onsubmit = (event: SubmitEvent) => {
+		const selectedText = document.getElementById("mocksiSelectedText");
+		// @ts-ignore I don't know why the value property is no inside the target object
+		const newValue = event.target?.value;
+		const parentElement = selectedText?.parentElement;
+		selectedText?.parentElement?.replaceChild(
+			document.createTextNode(newValue),
+			selectedText,
+		);
+		parentElement?.normalize();
+	};
+
+	//@ts-ignore
+	ndiv.value = value;
+	ndiv.id = "mocksiTextArea";
+	ndiv.autofocus = true;
+	return ndiv;
+}
+
+function onDoubleClickText(event: MouseEvent) {
+	const previousSelectedText = document.getElementById("mocksiSelectedText");
+	// @ts-ignore MouseEvent typing seems incomplete
+	if (event?.toElement?.nodeName !== "TEXTAREA") {
+		if (previousSelectedText) {
+			const parentElement = previousSelectedText?.parentElement;
+			// cancel previous input.
+			previousSelectedText?.parentElement?.replaceChild(
+				document.createTextNode(previousSelectedText.innerText),
+				previousSelectedText,
+			);
+			parentElement?.normalize();
+		}
+		const targetedElement: HTMLElement = event.target as HTMLElement;
+		const { startOffset, endOffset } =
+			window.getSelection()?.getRangeAt(0) || {};
+		if (startOffset !== undefined && endOffset !== undefined) {
+			applyEditor(targetedElement, window.getSelection(), event.shiftKey);
+			document.getElementById("mocksiTextArea")?.focus();
+		} else {
+			console.log("ERROR! no offset detected", targetedElement);
+		}
+	}
+}
+
 function initial() {
 	const rootDiv =
 		document.getElementById("extension-root") || document.createElement("div");
@@ -11,6 +175,14 @@ function initial() {
 }
 
 document.addEventListener("DOMContentLoaded", initial);
+
+export const setEditorMode = (turnOn: boolean) => {
+	if (turnOn) {
+		document.body.addEventListener("dblclick", onDoubleClickText);
+	} else {
+		document.body.removeEventListener("dblclick", onDoubleClickText);
+	}
+};
 
 chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
 	const extensionRoot = document.getElementById("extension-root");
