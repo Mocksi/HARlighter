4e3149a6b113c354422469cff6f44682471b0ad1
nicolaschapur <nicolaschapur@sirius.com.ar>
2024-06-05 11:07:48 -0300

    [NC]: removes redundant id assignment

--- apps/mocksi-lite/pages/tab-selector.tsx
+++ apps/mocksi-lite/pages/tab-selector.tsx
@@ -4,7 +4,6 @@ import TabSelector from "./TabSelector";
 document.addEventListener("DOMContentLoaded", () => {
 	const rootDiv = document.getElementById("tab-selector-container");
 	if (!rootDiv) return;
-	rootDiv.id = "tab-selector-container";
 	const reactRoot = ReactDOM.createRoot(rootDiv);
 	reactRoot.render(<TabSelector />);
 });
