52313240b022cba749dc3ebc83bb62ebf7d22b5a
nicolaschapur <nicolaschapur@sirius.com.ar>
2024-06-05 10:54:18 -0300

    [NC]: biome fixes

--- apps/mocksi-lite/pages/TabSelector.tsx
+++ apps/mocksi-lite/pages/TabSelector.tsx
@@ -1,67 +1,81 @@
 import "./tab-selector.css";
-import {useEffect, useState} from "react";
+import { Fragment, useEffect, useState } from "react";
 import chevronDown from "../public/chevron-down.png";
 
 const TabSelector = () => {
-  const [tabs, setTabs] = useState<chrome.tabs.Tab[]>([]);
-  const [selected, setSelected] = useState<string>('Select');
-  const [open, setOpen] = useState(false);
-  // const activeTab = tabs.find(t => t.active);
+	const [tabs, setTabs] = useState<chrome.tabs.Tab[]>([]);
+	const [selected, setSelected] = useState<string>("Select");
+	const [open, setOpen] = useState(false);
+	// const activeTab = tabs.find(t => t.active);
 
-  useEffect(() => {
-    chrome.tabs.query({}, (result) => setTabs(result));
-  }, []);
+	useEffect(() => {
+		chrome.tabs.query({}, (result) => setTabs(result));
+	}, []);
 
-  const handleChange = (tab: chrome.tabs.Tab) => {
-    setSelected(tab.title ?? 'Select');
-    setOpen(false);
+	const handleChange = (tab: chrome.tabs.Tab) => {
+		setSelected(tab.title ?? "Select");
+		setOpen(false);
 
-    const tabIdValue = tab.id?.toString() ?? '';
-    const tabId = Number.parseInt(tabIdValue, 10);
-    const message = "tabSelected";
+		const tabIdValue = tab.id?.toString() ?? "";
+		const tabId = Number.parseInt(tabIdValue, 10);
+		const message = "tabSelected";
 
-    chrome.runtime.sendMessage({ tabId, message }, (response) => {
-      if (response?.status !== "success") {
-        console.error("Failed to send message to background script");
-        return;
-      }
-    });
-    window.close();
-  };
+		chrome.runtime.sendMessage({ tabId, message }, (response) => {
+			if (response?.status !== "success") {
+				console.error("Failed to send message to background script");
+				return;
+			}
+		});
+		window.close();
+	};
 
-  return (
-    <>
-      <div className="tab-selector-header">
-        <div className="close-button" onClick={() => window.close()}>
-          <img src="../public/close-icon.png" alt="close-icon"/>
-        </div>
-        <img src="../public/menu-icon.png" alt="menu-icon"/>
-      </div>
-      <div className="tab-selector-content">
-        <label className="localize">
-          Select a Tab to Use With Mocksi
-        </label>
-        <div className={'dropdown'} onClick={() => setOpen(!open)}>
-          {selected}
-          <img src={chevronDown} alt={"chevronDown"} />
-        </div>
-        {
-          open &&
-          <div className={"dropdown-menu"}>
-            {
-              tabs.map(tab =>
-                <>
-                  <div className={"dropdown-item"} onClick={() => handleChange(tab)}>
-                    /{tab.title}
-                  </div>
-                  <div className={'separator'} />
-                </>
-              )}
-          </div>
-        }
-      </div>
-    </>
-  )
-}
+	return (
+		<>
+			<div className="tab-selector-header">
+				<div
+					className="close-button"
+					onClick={() => window.close()}
+					onKeyUp={(event) => {
+						event.key === "Escape" && window.close();
+					}}
+				>
+					<img src="../public/close-icon.png" alt="close-icon" />
+				</div>
+				<img src="../public/menu-icon.png" alt="menu-icon" />
+			</div>
+			<div className="tab-selector-content">
+				<label className="localize">Select a Tab to Use With Mocksi</label>
+				<div
+					className={"dropdown"}
+					onClick={() => setOpen(!open)}
+					onKeyUp={(event) => {
+						event.key === "Escape" && window.close();
+					}}
+				>
+					{selected}
+					<img src={chevronDown} alt={"chevronDown"} />
+				</div>
+				{open && (
+					<div className={"dropdown-menu"}>
+						{tabs.map((tab) => (
+							<Fragment key={`dropdown-tab-menu-${tab.id}`}>
+								<div
+									className={"dropdown-item"}
+									onClick={() => handleChange(tab)}
+									onKeyUp={(event) => {
+										event.key === "Escape" && window.close();
+									}}
+								>
+									/{tab.title}
+								</div>
+								<div className={"separator"} />
+							</Fragment>
+						))}
+					</div>
+				)}
+			</div>
+		</>
+	);
+};
 
 export default TabSelector;
--- apps/mocksi-lite/pages/tab-selector.tsx
+++ apps/mocksi-lite/pages/tab-selector.tsx
@@ -1,10 +1,10 @@
 import ReactDOM from "react-dom/client";
 import TabSelector from "./TabSelector";
 
-document.addEventListener('DOMContentLoaded', function () {
-  const rootDiv = document.getElementById("tab-selector-container")
-  if (!rootDiv) return;
-  rootDiv.id = "tab-selector-container";
-  const reactRoot = ReactDOM.createRoot(rootDiv);
-  reactRoot.render(<TabSelector />)
+document.addEventListener("DOMContentLoaded", () => {
+	const rootDiv = document.getElementById("tab-selector-container");
+	if (!rootDiv) return;
+	rootDiv.id = "tab-selector-container";
+	const reactRoot = ReactDOM.createRoot(rootDiv);
+	reactRoot.render(<TabSelector />);
 });
