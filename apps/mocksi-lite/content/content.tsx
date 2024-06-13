25f9981ca116199eb6e860ce5a480361467f4a21
nicolaschapur <95246792+nicolaschapur@users.noreply.github.com>
2024-06-13 13:44:03 -0300

    [NC]: Root position fix for initial state (#50)
    * [NC]: Adds root position fix for initial state
    
    * [NC]: format fix
    
    * [NC]: pop up render after signout fix

--- apps/mocksi-lite/content/content.tsx
+++ apps/mocksi-lite/content/content.tsx
@@ -1,6 +1,12 @@
 import ReactDOM from "react-dom/client";
 import MocksiRollbar from "../MocksiRollbar";
-import { STORAGE_CHANGE_EVENT, STORAGE_KEY } from "../consts";
+import {
+	MOCKSI_RECORDING_STATE,
+	RecordingState,
+	STORAGE_CHANGE_EVENT,
+	STORAGE_KEY,
+} from "../consts";
+import { setRootPosition } from "../utils";
 import ContentApp from "./ContentApp";
 
 let root: ReactDOM.Root;
@@ -23,13 +29,19 @@ chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
 		}
 		root = ReactDOM.createRoot(extensionRoot);
 		chrome.storage.local.get(STORAGE_KEY).then((value) => {
-			const { email } = JSON.parse(value[STORAGE_KEY] || {});
+			const { email } = JSON.parse(value[STORAGE_KEY] || "{}");
+			const recordingState = localStorage.getItem(
+				MOCKSI_RECORDING_STATE,
+			) as RecordingState | null;
 			if (email) {
 				// we need to initialize recordingState if there's none.
-				!localStorage.getItem("mocksi-recordingState") &&
-					localStorage.setItem("mocksi-recordingState", "READY");
-				root.render(<ContentApp isOpen={true} email={email || ""} />);
+				!recordingState &&
+					localStorage.setItem(MOCKSI_RECORDING_STATE, RecordingState.READY);
 			}
+			if (recordingState) {
+				setRootPosition(recordingState);
+			}
+			root.render(<ContentApp isOpen={true} email={email || ""} />);
 		});
 	}
 	sendResponse({ status: "success" });
