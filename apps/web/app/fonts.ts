934ac7b4392c2196064284e5f068cbcc6fdbf5bd
G Maldonado <elg0nz@gmail.com>
2024-06-13 10:30:30 -0700

    remove outdated apps (#51)

--- apps/web/app/fonts.ts
+++
@@ -1,5 +0,0 @@
-import { Spline_Sans, Piazzolla } from "next/font/google";
-
-export const splineSans = Spline_Sans({ subsets: ["latin"]})
-
-export const titleFont = Piazzolla({ subsets: ["latin"], weight: '900'})
