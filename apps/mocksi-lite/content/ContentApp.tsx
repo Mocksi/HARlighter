c10128944898063f1431127606676f4d0423403c
elg0nz <elg0nz@gmail.com>
2024-06-20 19:14:40 -0700

    add shadow-dom to prevent mocksi controls from being affected by changes in the main content

--- apps/mocksi-lite/content/ContentApp.tsx
+++ apps/mocksi-lite/content/ContentApp.tsx
@@ -12,6 +12,7 @@ import { setEditorMode } from "./EditMode/editMode";
 import { getHighlighter } from "./EditMode/highlighter";
 import Popup from "./Popup";
 import { RecordButton } from "./RecordButton";
+import useShadow from 'use-shadow-dom';
 
 interface ContentProps {
 	isOpen?: boolean;
@@ -34,7 +35,7 @@ const recordingLabel = (currentStatus: RecordingState) => {
 	}
 };
 
-export default function ContentApp({ isOpen, email }: ContentProps) {
+function ShadowContentApp({ isOpen, email }: ContentProps) {
 	const [isDialogOpen, setIsDialogOpen] = useState(isOpen || false);
 	const [areChangesHighlighted, setAreChangesHighlighted] = useState(true);
 	const [state, setState] = useState<RecordingState>(
@@ -165,4 +166,30 @@ export default function ContentApp({ isOpen, email }: ContentProps) {
 			)}
 		</div>
 	);
+}
+
+const extractStyles = (): string => {
+    let styles = '';
+    const styleSheets = Array.from(document.styleSheets) as CSSStyleSheet[];
+
+	for (const sheet of styleSheets) {
+        try {
+            if (sheet.cssRules) {
+                const cssRules = Array.from(sheet.cssRules) as CSSRule[];
+				for(const rule of cssRules) {
+					styles += rule.cssText;
+				}
+            }
+        } catch (e) {
+            console.error('Error accessing stylesheet:', e);
+        }
+    }
+
+    return styles;
+};
+
+
+export default function ContentApp({ isOpen, email }: ContentProps) {
+	const styles = extractStyles();
+	return useShadow(<ShadowContentApp isOpen={isOpen} email={email} />,  [], {styleContent: styles});
 }
