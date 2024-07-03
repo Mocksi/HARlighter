1401b76bb0e4f6795add8c830320d9f2a9c100c8
Brennen Denomme <brennenkdenomme@gmail.com>
2024-07-03 16:33:40 -0400

    setup draggable

--- apps/mocksi-lite/content/Popup/index.tsx
+++ apps/mocksi-lite/content/Popup/index.tsx
@@ -6,6 +6,7 @@ import Divider from "./Divider";
 import Footer from "./Footer";
 import Header from "./Header";
 import RecordDemo from "./RecordDemo";
+import Draggable from "react-draggable";
 
 interface PopupProps {
 	close: () => void;
@@ -36,7 +37,16 @@ const Popup = ({ close, setState, state, email }: PopupProps) => {
 		}
 	};
 
+	const onDragStop = (...args: any[]) => {
+		console.log('drag stop', args)
+	}
+
+	const onDragStart = (...args: any[]) => {
+		console.log('drag stop', args)
+	}
+
 	return (
+		<Draggable handle=".drag-handle" onStart={onDragStart} onStop={onDragStop}>
 		<div
 			className={
 				"w-[500px] h-[596px] shadow-lg rounded-lg m-4 bg-white flex flex-col justify-between"
@@ -55,6 +65,7 @@ const Popup = ({ close, setState, state, email }: PopupProps) => {
 				</div>
 			)}
 		</div>
+		</Draggable>
 	);
 };
 
