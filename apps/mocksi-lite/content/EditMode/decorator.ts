6ae20aa69b59848bfa6e23156822edcdefbb8ac0
Brennen Denomme <brennenkdenomme@gmail.com>
2024-07-23 17:54:44 -0400

    style highlighter and editor (#103)
    * highlighting styles
    
    * styled text area closer to figma
    
    * biome fixes
    
    ---------
    
    Co-authored-by: elg0nz <elg0nz@gmail.com>

--- apps/mocksi-lite/content/EditMode/decorator.ts
+++ apps/mocksi-lite/content/EditMode/decorator.ts
@@ -1,4 +1,5 @@
 import { applyChanges, cancelEditWithoutChanges } from "./actions";
+import { applyStyles } from "./utils";
 
 // function to decorate the portion of TextNode with the textArea to edit the content
 // functions parameter is to add some extra functionality at the moment of submitting or cancel.
@@ -33,24 +34,23 @@ function injectTextArea(
 ) {
 	const ndiv = document.createElement("textarea");
 	ndiv.setAttribute("tabindex", "-1");
+
+	const padding = "5px";
 	const elementStyle = {
-		width: width ? "120%" : "150%",
-		height: "100%",
-		border: "1px solid red",
+		width: `calc(120% + ${padding})`, // TODO: Make this dynamic based off the length of the content
+		height: `calc(100% + ${padding} + ${padding})`,
+		border: "1px solid #33B8EA",
+		padding: padding,
+		borderRadius: "2px",
 		position: "absolute",
-		top: "0",
-		left: "0",
+		top: `-${padding}`,
+		left: `-${padding}`,
 		zIndex: "999",
-		background: "#f0f8ffa8",
+		background: "#FFFFFF",
+		resize: "none",
 	};
-	ndiv.style.width = elementStyle.width;
-	ndiv.style.height = elementStyle.height;
-	ndiv.style.border = elementStyle.border;
-	ndiv.style.position = elementStyle.position;
-	ndiv.style.top = elementStyle.top;
-	ndiv.style.left = elementStyle.left;
-	ndiv.style.zIndex = elementStyle.zIndex;
-	ndiv.style.background = elementStyle.background;
+	applyStyles(ndiv, elementStyle);
+
 	ndiv.onkeydown = (event: KeyboardEvent) => {
 		if (event.key === "Enter" && !event.shiftKey) {
 			if (!event.repeat) {
---
+++ apps/mocksi-lite/content/EditMode/utils.ts
@@ -0,0 +1,8 @@
+export const applyStyles = (
+	element: HTMLElement,
+	styles: Record<string, string>,
+) => {
+	for (const [key, value] of Object.entries(styles)) {
+		element.style.setProperty(key, value);
+	}
+};
