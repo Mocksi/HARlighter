fd492a9d89517bf0f40c135d74523d29191b3dd8
elg0nz <elg0nz@gmail.com>
2024-07-26 18:28:30 -0700

    isolate RecordingToast and EditToast via IframeWrapper

--- apps/mocksi-lite/content/Toast/EditToast.tsx
+++ apps/mocksi-lite/content/Toast/EditToast.tsx
@@ -5,6 +5,7 @@ import { loadRecordingId, recordingLabel } from "../../utils";
 import { AppEvent, AppStateContext } from "../AppStateContext";
 import { setEditorMode } from "../EditMode/editMode";
 import { getHighlighter } from "../EditMode/highlighter";
+import IframeWrapper from "../IframeWrapper";
 import Toast from "./index";
 
 const EditToast = () => {
@@ -34,49 +35,58 @@ const EditToast = () => {
 		dispatch({ event: AppEvent.CANCEL_EDITING });
 	};
 
+	const iframeStyle = {
+		position: "relative",
+		zIndex: 9999998,
+		border: "none",
+		width: "355px",
+	};
+
 	return (
-		<Toast className="mw-mt-3 min-w-64 mw-p-3 mw-flex mw-flex-row mw-items-center mw-gap-6">
-			<div
-				className="mw-cursor-pointer"
-				onClick={() => {
-					handleCancel();
-				}}
-				onKeyUp={async (event) => {
-					if (event.key === "Escape") {
+		<IframeWrapper style={iframeStyle}>
+			<Toast className="mw-mt-3 min-w-64 mw-p-3 mw-flex mw-flex-row mw-items-center mw-gap-6">
+				<div
+					className="mw-cursor-pointer"
+					onClick={() => {
 						handleCancel();
-					}
-				}}
-			>
-				<img src={closeIcon} alt="closeIcon" />
-			</div>
-			<div className="mw-flex mw-flex-col mw-gap-2">
-				<TextField variant={"title"}>{recordingLabel(state)}</TextField>
-				<div className="mw-flex mw-gap-2 mw-items-center">
-					<input
-						checked={areChangesHighlighted}
-						onChange={() => onChecked()}
-						type="checkbox"
-						className="mw-h-5 mw-w-5 !rounded-lg"
-					/>
-					<div className="mw-text-[13px] leading-[15px]">
-						Highlight All Previous Changes
+					}}
+					onKeyUp={async (event) => {
+						if (event.key === "Escape") {
+							handleCancel();
+						}
+					}}
+				>
+					<img src={closeIcon} alt="closeIcon" />
+				</div>
+				<div className="mw-flex mw-flex-col mw-gap-2">
+					<TextField variant={"title"}>{recordingLabel(state)}</TextField>
+					<div className="mw-flex mw-gap-2 mw-items-center">
+						<input
+							checked={areChangesHighlighted}
+							onChange={() => onChecked()}
+							type="checkbox"
+							className="mw-h-5 mw-w-5 !rounded-lg"
+						/>
+						<div className="mw-text-[13px] leading-[15px]">
+							Highlight All Previous Changes
+						</div>
 					</div>
 				</div>
-			</div>
-			<div
-				className="mw-cursor-pointer mw-text-[#009875]"
-				onClick={async () => {
-					handleSave();
-				}}
-				onKeyUp={async (event) => {
-					if (event.key === "Enter") {
+				<div
+					className="mw-cursor-pointer mw-text-[#009875]"
+					onClick={async () => {
 						handleSave();
-					}
-				}}
-			>
-				Done
-			</div>
-		</Toast>
+					}}
+					onKeyUp={async (event) => {
+						if (event.key === "Enter") {
+							handleSave();
+						}
+					}}
+				>
+					Done
+				</div>
+			</Toast>
+		</IframeWrapper>
 	);
 };
 
