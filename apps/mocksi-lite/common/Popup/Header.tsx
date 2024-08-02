d8da56f4363835bd6c44af3244e66828509939e0
Kayla Fitzsimmons <kayla@Kaylas-MacBook-Pro.local>
2024-08-02 13:00:58 -0700

    [format] make Popup header more readable

--- apps/mocksi-lite/common/Popup/Header.tsx
+++ apps/mocksi-lite/common/Popup/Header.tsx
@@ -14,10 +14,10 @@ interface HeaderProps {
 	onSettings?: () => void;
 }
 
-const Header = ({ subtitle, close, onSettings, onGoBack }: HeaderProps) => {
+const Header = ({ close, onGoBack, onSettings, subtitle }: HeaderProps) => {
 	return (
 		<div>
-			<div className="mw-h-[36px] mw-flex mw-items-center mw-flex-row mw-px-2">
+			<div className="mw-flex mw-flex-row mw-items-center mw-px-2 mw-h-[36px]">
 				<div
 					className="mw-cursor-pointer"
 					onClick={close}
@@ -25,40 +25,40 @@ const Header = ({ subtitle, close, onSettings, onGoBack }: HeaderProps) => {
 						event.key === "Escape" && close();
 					}}
 				>
-					<img src={closeIcon} alt="closeIcon" />
+					<img alt="closeIcon" src={closeIcon} />
 				</div>
-				<div className="mw-w-full mw-mr-[20px] mw-flex mw-justify-center drag-handle">
-					<img src={menuIcon} draggable="false" alt="menuIcon" />
+				<div className="mw-flex mw-justify-center mw-mr-[20px] mw-w-full drag-handle">
+					<img alt="menuIcon" draggable="false" src={menuIcon} />
 				</div>
 			</div>
 			<Divider />
 			<div className="mw-flex mw-justify-center mw-items-center mw-mt-5">
 				{onGoBack && (
 					<div
-						className="mw-absolute mw-left-9 mw-cursor-pointer"
+						className="mw-left-9 mw-absolute mw-cursor-pointer"
 						onClick={onGoBack}
 						onKeyUp={(event) => {
 							event.key === "Enter" && onGoBack();
 						}}
 					>
-						<img src={backIcon} alt={"backIcon"} className="mw-w-[16px]" />
+						<img alt={"backIcon"} className="mw-w-[16px]" src={backIcon} />
 					</div>
 				)}
 				<div className="mw-flex mw-flex-col mw-justify-center mw-items-center mw-gap-[5px]">
 					<div>
-						<img src={labeledIcon} alt={"labeledIcon"} />
+						<img alt={"labeledIcon"} src={labeledIcon} />
 					</div>
 					{subtitle && <TextField variant="title">{subtitle}</TextField>}
 				</div>
 				{onSettings && (
 					<div
-						className="mw-absolute mw-right-9 mw-cursor-pointer"
+						className="mw-right-9 mw-absolute mw-cursor-pointer"
 						onClick={onSettings}
 						onKeyUp={(event) => {
 							event.key === "Enter" && onSettings();
 						}}
 					>
-						<img src={trashIcon} alt={"trashIcon"} />
+						<img alt={"trashIcon"} src={trashIcon} />
 					</div>
 				)}
 			</div>
