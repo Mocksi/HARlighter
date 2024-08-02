68ccd9cd2921a621c75e8e9475e8eb08241ae62e
Kayla Fitzsimmons <kayla@Kaylas-MacBook-Pro.local>
2024-08-02 12:59:52 -0700

    [fix] minimal change for rounded icon buttons

--- apps/mocksi-lite/common/Button.tsx
+++ apps/mocksi-lite/common/Button.tsx
@@ -7,30 +7,31 @@ export enum Variant {
 }
 interface ButtonProps {
 	children: ReactNode;
-	onClick: () => void;
-	variant?: Variant;
 	className?: string;
 	disabled?: boolean;
+	variant?: Variant;
+	onClick: () => void;
 }
 
 const getButtonStyles = (variant: Variant) => {
 	switch (variant) {
+		case Variant.icon:
+			return "mw-bg-[#E8F3EC] mw-border-[#E8F3EC] mw-py-0.75 mw-px-2.5 !mw-max-h-[42px] !mw-h-[42px]";
 		case Variant.primary:
 			return "mw-bg-[#E8F3EC] mw-border-[#E8F3EC] mw-px-6";
-		case Variant.icon:
-			return "mw-bg-[#E8F3EC] mw-border-[#E8F3EC] mw-p-3 !mw-max-h-[42px] !mw-h-[42px]";
 		case Variant.secondary:
 			return "mw-border-[#009875] mw-px-6";
 		default:
 			return "mw-bg-[#E8F3EC] mw-border-[#E8F3EC] mw-px-6";
 	}
 };
+
 const Button = ({
 	children,
-	onClick,
-	variant = Variant.primary,
 	className,
 	disabled,
+	onClick,
+	variant = Variant.primary,
 }: ButtonProps) => {
 	const styles = getButtonStyles(variant);
 	const buttonClassNames = `mw-border mw-text-[#009875] mw-w-fit !mw-min-h-[42px] mw-rounded-full mw-flex mw-items-center mw-justify-center ${
