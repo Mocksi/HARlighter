d822c855fe3e7036a9d2c80e4d006564826a5dd0
Kayla Fitzsimmons <kayla@Kaylas-MacBook-Pro.local>
2024-08-05 11:59:58 -0700

    cleanup

--- apps/mocksi-lite/common/Popup/Draggable.tsx
+++ apps/mocksi-lite/common/Popup/Draggable.tsx
@@ -1,47 +1,49 @@
-import type React from "react";
 import { type MouseEventHandler, useEffect, useRef, useState } from "react";
+import React from "react";
 
 function Draggable({
-	children,
-	className,
+  children,
+  className,
 }: {
-	children: React.ReactNode;
-	className?: string;
+  children: React.ReactNode;
+  className?: string;
 }) {
-	const [pressed, setPressed] = useState(false);
-	const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [pressed, setPressed] = useState(false);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
 
-	const ref = useRef<HTMLDivElement>(null);
+  const ref = useRef<HTMLDivElement>(null);
 
-	useEffect(() => {
-		if (ref.current?.style) {
-			ref.current.style.transform = `translate(${position.x}px, ${position.y}px)`;
-		}
-	}, [position]);
+  useEffect(() => {
+    if (ref.current?.style) {
+      ref.current.style.transform = `translate(${position.x}px, ${position.y}px)`;
+    }
+  }, [position]);
 
-	const onMouseMove: MouseEventHandler<HTMLButtonElement | HTMLDivElement> = (
-		event,
-	) => {
-		if (pressed) {
-			setPosition({
-				x: position.x + event.movementX,
-				y: position.y + event.movementY,
-			});
-		}
-	};
+  const onMouseMove: MouseEventHandler<HTMLButtonElement | HTMLDivElement> = (
+    event,
+  ) => {
+    if (pressed) {
+      setPosition({
+        x: position.x + event.movementX,
+        y: position.y + event.movementY,
+      });
+    }
+  };
 
-	return (
-		<div
-			className={className}
-			onMouseDown={() => setPressed(true)}
-			onMouseEnter={() => setPressed(false)}
-			onMouseMove={onMouseMove}
-			onMouseUp={() => setPressed(false)}
-			ref={ref}
-		>
-			{children}
-		</div>
-	);
+  return (
+    <div
+      className={className}
+      draggable
+      onDragExit={(e) => console.log(e)}
+      onMouseDown={() => setPressed(true)}
+      onMouseEnter={() => setPressed(false)}
+      onMouseMove={onMouseMove}
+      onMouseUp={() => setPressed(false)}
+      ref={ref}
+    >
+      {children}
+    </div>
+  );
 }
 
 export default Draggable;
