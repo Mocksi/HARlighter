b2fbd3f3f4b6b249af812d3e242ec07a51b4572b
Kayla Fitzsimmons <kayla@Kaylas-MacBook-Pro.local>
2024-08-06 23:17:02 -0700

    remove class

--- apps/mocksi-lite/common/Divider.tsx
+++ apps/mocksi-lite/common/Divider.tsx
@@ -1,5 +1,3 @@
-const Divider = () => (
-	<div className="mw-bg-[#E5E2E1] mw-h-px mw-overflow-clip" />
-);
+const Divider = () => <div className="mw-bg-[#E5E2E1] mw-h-px" />;
 
 export default Divider;
