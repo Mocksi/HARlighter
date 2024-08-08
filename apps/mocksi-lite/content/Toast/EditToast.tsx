c3aae1ca16d3b295060d5db4d7bcdce4ba385627
Brennen Denomme <brennenkdenomme@gmail.com>
2024-08-08 13:02:51 -0400

    changed the way we use observe url (#130)
    * changed the way we use observe url
    
    * formatting and ignore biome

--- apps/mocksi-lite/content/Toast/EditToast.tsx
+++ apps/mocksi-lite/content/Toast/EditToast.tsx
@@ -10,6 +10,7 @@ import {
 import {
 	getAlterations,
 	loadAlterations,
+	loadPreviousModifications,
 	persistModifications,
 	recordingLabel,
 	sendMessage,
@@ -65,7 +66,9 @@ const EditToast = ({ initialReadOnlyState }: EditToastProps) => {
 	);
 	const [alterations, setAlterations] = useState<Alteration[]>([]);
 	const [recordingId, setRecordingId] = useState<string | null>(null);
+	const [url, setUrl] = useState<string>(document.location.href);
 
+	// biome-ignore lint/correctness/useExhaustiveDependencies: only run this on mount
 	useEffect(() => {
 		// get alterations that were set in DemoItem.tsx and load them into state
 		chrome.storage.local
@@ -78,11 +81,13 @@ const EditToast = ({ initialReadOnlyState }: EditToastProps) => {
 				}
 				setRecordingId(recordingId);
 
-				const alterations = result[MOCKSI_ALTERATIONS] || [];
-				setAlterations(alterations);
+				const storedAlterations = result[MOCKSI_ALTERATIONS];
+				setAlterations(storedAlterations);
 
 				// TODO: would be nice if it was like loadAlterations(alterations, { withHighlights: true })
-				loadAlterations(alterations, { withHighlights: true });
+				loadAlterations(storedAlterations, {
+					withHighlights: areChangesHighlighted,
+				});
 
 				setupEditor();
 			})
@@ -91,11 +96,20 @@ const EditToast = ({ initialReadOnlyState }: EditToastProps) => {
 			});
 	}, []);
 
+	// Each time the URL updates we want to remove the existing highlights, and reload the alterations onto the page
+	// biome-ignore lint/correctness/useExhaustiveDependencies: we dont use the url but want to run this whenever it changes
+	useEffect(() => {
+		getHighlighter().removeHighlightNodes();
+		loadAlterations(alterations, { withHighlights: areChangesHighlighted });
+	}, [url]);
+
 	const setupEditor = async () => {
 		sendMessage("attachDebugger");
 
+		// Whenever the url changes, we want to update the url in state which triggers the
+		// use effect that removes the highlights and reloads the alterations
 		observeUrlChange(() => {
-			loadAlterations(alterations, { withHighlights: true });
+			setUrl(document.location.href);
 		});
 
 		const results = await chrome.storage.local.get([MOCKSI_READONLY_STATE]);
