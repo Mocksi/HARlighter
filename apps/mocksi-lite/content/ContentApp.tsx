29f7c5489b90171f71a39ebbeaf17f49cecdf3da
Kayla Fitzsimmons <kayla@Kaylas-MacBook-Pro.local>
2024-08-09 09:30:47 -0700

    move Draggable component up to parent

--- apps/mocksi-lite/content/ContentApp.tsx
+++ apps/mocksi-lite/content/ContentApp.tsx
@@ -1,13 +1,14 @@
 import { useContext, useEffect, useState } from "react";
 import useShadow from "use-shadow-dom";
 import type { Recording } from "../background";
+import Draggable from "../common/Popup/Draggable";
 import { MOCKSI_LAST_PAGE_DOM } from "../consts";
 import { extractStyles, innerHTMLToJson, logout } from "../utils";
 import {
-	AppEvent,
-	AppState,
-	AppStateContext,
-	AppStateProvider,
+  AppEvent,
+  AppState,
+  AppStateContext,
+  AppStateProvider,
 } from "./AppStateContext";
 import CreatePopup from "./CreatePopup";
 import ListPopup from "./ListPopup";
@@ -19,117 +20,117 @@ import PlayToast from "./Toast/PlayToast";
 import RecordingToast from "./Toast/RecordingToast";
 
 import(
-	/* webpackChunkName: "content_content_css" */
-	/* webpackPrefetch: true */
-	/* webpackPreload: true */
-	"./content.css"
+  /* webpackChunkName: "content_content_css" */
+  /* webpackPrefetch: true */
+  /* webpackPreload: true */
+  "./content.css"
 );
 import(
-	/* webpackChunkName: "content_base_css" */
-	/* webpackPrefetch: true */
-	/* webpackPreload: true */
-	"./base.css"
+  /* webpackChunkName: "content_base_css" */
+  /* webpackPrefetch: true */
+  /* webpackPreload: true */
+  "./base.css"
 );
 import(
-	/* webpackChunkName: "content_spinner_css" */
-	/* webpackPrefetch: true */
-	/* webpackPreload: true */
-	"./spinner.css"
+  /* webpackChunkName: "content_spinner_css" */
+  /* webpackPrefetch: true */
+  /* webpackPreload: true */
+  "./spinner.css"
 );
 
 interface ContentProps {
-	isOpen?: boolean;
-	email?: string;
-	initialState?: {
-		recordings?: Recording[];
-		readOnly?: boolean;
-	};
+  email?: string;
+  initialState?: {
+    readOnly?: boolean;
+    recordings?: Recording[];
+  };
+  isOpen?: boolean;
 }
 
-function ShadowContentApp({ isOpen, email, initialState }: ContentProps) {
-	const { state, dispatch } = useContext(AppStateContext);
-	const [isDialogOpen, setIsDialogOpen] = useState(isOpen || false);
+function ShadowContentApp({ email, initialState, isOpen }: ContentProps) {
+  const { dispatch, state } = useContext(AppStateContext);
+  const [isDialogOpen, setIsDialogOpen] = useState(isOpen || false);
 
-	useEffect(() => {
-		let dom_as_json = "";
-		try {
-			dom_as_json = innerHTMLToJson(document.body.innerHTML);
-		} catch (e) {
-			console.error("Error setting last page dom:", e);
-		}
-		chrome.storage.local.set({ [MOCKSI_LAST_PAGE_DOM]: dom_as_json });
-	});
+  useEffect(() => {
+    let dom_as_json = "";
+    try {
+      dom_as_json = innerHTMLToJson(document.body.innerHTML);
+    } catch (e) {
+      console.error("Error setting last page dom:", e);
+    }
+    chrome.storage.local.set({ [MOCKSI_LAST_PAGE_DOM]: dom_as_json });
+  });
 
-	const closeDialog = () => setIsDialogOpen(false);
+  const closeDialog = () => setIsDialogOpen(false);
 
-	const handleOnChat = () => {
-		dispatch({ event: AppEvent.START_CHAT });
-	};
+  const handleOnChat = () => {
+    dispatch({ event: AppEvent.START_CHAT });
+  };
 
-	const handleOnLogout = () => {
-		logout();
-	};
+  const handleOnLogout = () => {
+    logout();
+  };
 
-	if (!isDialogOpen) {
-		return null;
-	}
+  // if (!isDialogOpen) {
+  // 	return null;
+  // }
 
-	const renderContent = () => {
-		const popupProps = {
-			onClose: closeDialog,
-			email,
-			onChat: handleOnChat,
-			onLogout: handleOnLogout,
-		};
-		switch (state) {
-			case AppState.EDITING:
-				return <EditToast initialReadOnlyState={initialState?.readOnly} />;
-			case AppState.PLAY:
-				return <PlayToast close={closeDialog} />;
-			case AppState.CHAT:
-				return <ChatToast onChangeState={() => {}} close={closeDialog} />;
-			case AppState.RECORDING:
-			case AppState.ANALYZING:
-				return <RecordingToast close={closeDialog} />;
-			case AppState.CREATE:
-				return <CreatePopup onClose={closeDialog} />;
-			case AppState.LIST:
-				return <ListPopup {...popupProps} />;
-			case AppState.SETTINGS:
-				return <SettingsPopup {...popupProps} />;
-			case AppState.READYTORECORD:
-				return <ReadyToRecordPopup {...popupProps} />;
-			case AppState.UNAUTHORIZED:
-			case AppState.INIT:
-				// When initializing the application and loading state we want to show nothing, potentially this is a loading UI in the future
-				return null;
-			default:
-				return <ListPopup {...popupProps} />;
-		}
-	};
+  const renderContent = () => {
+    const popupProps = {
+      email,
+      onChat: handleOnChat,
+      onClose: closeDialog,
+      onLogout: handleOnLogout,
+    };
+    switch (state) {
+      case AppState.ANALYZING:
+      case AppState.RECORDING:
+        return <RecordingToast close={closeDialog} />;
+      case AppState.CHAT:
+        return <ChatToast close={closeDialog} onChangeState={() => {}} />;
+      case AppState.CREATE:
+        return <CreatePopup onClose={closeDialog} />;
+      case AppState.EDITING:
+        return <EditToast initialReadOnlyState={initialState?.readOnly} />;
+      case AppState.INIT:
+      case AppState.UNAUTHORIZED:
+        // When initializing the application and loading state we want to show nothing, potentially this is a loading UI in the future
+        return null;
+      case AppState.LIST:
+        return <ListPopup {...popupProps} />;
+      case AppState.PLAY:
+        return <PlayToast close={closeDialog} />;
+      case AppState.READYTORECORD:
+        return <ReadyToRecordPopup {...popupProps} />;
+      case AppState.SETTINGS:
+        return <SettingsPopup {...popupProps} />;
+      default:
+        return <ListPopup {...popupProps} />;
+    }
+  };
 
-	return renderContent();
+  return <Draggable> {renderContent()}</Draggable>;
 }
 
 export default function ContentApp({
-	isOpen,
-	email,
-	initialState,
+  email,
+  initialState,
+  isOpen,
 }: ContentProps) {
-	const styles = extractStyles(document.styleSheets);
-	return useShadow(
-		<AppStateProvider initialRecordings={initialState?.recordings}>
-			<div className="mcksi-frame-include">
-				<ShadowContentApp
-					isOpen={isOpen}
-					email={email}
-					initialState={initialState}
-				/>
-			</div>
-		</AppStateProvider>,
-		[],
-		{
-			styleContent: styles,
-		},
-	);
+  const styles = extractStyles(document.styleSheets);
+  return useShadow(
+    <AppStateProvider initialRecordings={initialState?.recordings}>
+      <div className="mcksi-frame-include">
+        <ShadowContentApp
+          email={email}
+          initialState={initialState}
+          isOpen={isOpen}
+        />
+      </div>
+    </AppStateProvider>,
+    [],
+    {
+      styleContent: styles,
+    },
+  );
 }
