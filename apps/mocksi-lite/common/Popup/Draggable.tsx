ff54de5ab357a1c01d065026821eed8f785aecd2
Kayla Fitzsimmons <kayla@Kaylas-MacBook-Pro.local>
2024-08-09 09:56:17 -0700

    add close popup back

--- apps/mocksi-lite/common/Popup/Draggable.tsx
+++ apps/mocksi-lite/common/Popup/Draggable.tsx
@@ -7,144 +7,148 @@ const topOffset = 20;
 const bottomOffset = 20;
 
 function Draggable({
-  children,
-  className,
+	children,
+	className,
+	isOpen,
+	onClose,
 }: {
-  children: React.ReactNode;
-  className?: string;
+	children: React.ReactNode;
+	className?: string;
+	isOpen: boolean;
+	onClose?: () => void;
 }) {
-  const [dragging, setDragging] = React.useState(false);
-  const [transform, setTransform] = React.useState({
-    x: 0,
-    y: 0,
-  });
+	const [dragging, setDragging] = React.useState(false);
+	const [transform, setTransform] = React.useState({
+		x: 0,
+		y: 0,
+	});
 
-  const initRef = React.useRef(true);
-  const dragElRef = React.useRef<HTMLDivElement>(null);
-  const prevTransform = React.useRef(transform);
+	const initRef = React.useRef(true);
+	const dragElRef = React.useRef<HTMLDivElement>(null);
+	const prevTransform = React.useRef(transform);
 
-  const persistTransform = React.useCallback(async () => {
-    if (
-      prevTransform.current.x !== transform.x ||
-      prevTransform.current.y !== transform.y
-    ) {
-      // x y transform validated before state is updated
-      await chrome.storage.local.set({
-        [MOCKSI_POPUP_LOCATION]: transform,
-      });
-      prevTransform.current = transform;
-    }
-  }, [transform]);
+	const persistTransform = React.useCallback(async () => {
+		if (
+			prevTransform.current.x !== transform.x ||
+			prevTransform.current.y !== transform.y
+		) {
+			// x y transform validated before state is updated
+			await chrome.storage.local.set({
+				[MOCKSI_POPUP_LOCATION]: transform,
+			});
+			prevTransform.current = transform;
+		}
+	}, [transform]);
 
-  const stopDragging = React.useCallback(async () => {
-    setDragging(false);
-  }, []);
+	const stopDragging = React.useCallback(async () => {
+		setDragging(false);
+	}, []);
 
-  const getValidTransform = (xMov = 0, yMov = 0, reqBothValid = false) => {
-    const bounds = dragElRef?.current?.getBoundingClientRect();
+	const getValidTransform = (xMov = 0, yMov = 0, reqBothValid = false) => {
+		const bounds = dragElRef?.current?.getBoundingClientRect();
 
-    // default values
-    let x = rightOffset;
-    let y = topOffset;
+		// default values
+		let x = rightOffset;
+		let y = topOffset;
 
-    if (bounds) {
-      // x bound checks
-      const rightBoundCheck =
-        bounds.x + xMov < window.innerWidth - rightOffset - bounds.width / 2;
-      const leftBoundCheck = bounds.x + xMov > leftOffset;
+		if (bounds) {
+			// x bound checks
+			const rightBoundCheck =
+				bounds.x + xMov < window.innerWidth - rightOffset - bounds.width / 2;
+			const leftBoundCheck = bounds.x + xMov > leftOffset;
 
-      // y bound checks
-      const bottomBoundCheck =
-        bounds.y + yMov < window.innerHeight - bottomOffset - bounds.height;
-      const topBoundCheck = bounds.y + yMov > topOffset;
+			// y bound checks
+			const bottomBoundCheck =
+				bounds.y + yMov < window.innerHeight - bottomOffset - bounds.height;
+			const topBoundCheck = bounds.y + yMov > topOffset;
 
-      const xNextValid = rightBoundCheck && leftBoundCheck;
-      const yNextValid = bottomBoundCheck && topBoundCheck;
+			const xNextValid = rightBoundCheck && leftBoundCheck;
+			const yNextValid = bottomBoundCheck && topBoundCheck;
 
-      if (xNextValid && yNextValid) {
-        x = transform.x + xMov;
-        y = transform.y + yMov;
-      }
+			if (xNextValid && yNextValid) {
+				x = transform.x + xMov;
+				y = transform.y + yMov;
+			}
 
-      if (!reqBothValid) {
-        x = xNextValid ? transform.x + xMov : transform.x;
-        y = yNextValid ? transform.y + yMov : transform.y;
-      }
-    }
+			if (!reqBothValid) {
+				x = xNextValid ? transform.x + xMov : transform.x;
+				y = yNextValid ? transform.y + yMov : transform.y;
+			}
+		}
 
-    return { x, y };
-  };
+		return { x, y };
+	};
 
-  function debounce<T, A>(fn: T, delay: number): (args?: A) => void {
-    let timeout: number;
-    return (args?: A) => {
-      clearTimeout(timeout);
-      timeout = window.setTimeout(() => {
-        if (typeof fn === "function") {
-          fn(args);
-        }
-      }, delay);
-    };
-  }
+	function debounce<T, A>(fn: T, delay: number): (args?: A) => void {
+		let timeout: number;
+		return (args?: A) => {
+			clearTimeout(timeout);
+			timeout = window.setTimeout(() => {
+				if (typeof fn === "function") {
+					fn(args);
+				}
+			}, delay);
+		};
+	}
 
-  const updateTransformOnDrag: React.MouseEventHandler<HTMLDivElement> =
-    debounce((event: React.MouseEvent) => {
-      if (dragging) {
-        const validTransform = getValidTransform(
-          event.movementX,
-          event.movementY,
-        );
-        setTransform(validTransform);
-      }
-    }, 10);
+	const updateTransformOnDrag: React.MouseEventHandler<HTMLDivElement> =
+		debounce((event: React.MouseEvent) => {
+			if (dragging) {
+				const validTransform = getValidTransform(
+					event.movementX,
+					event.movementY,
+				);
+				setTransform(validTransform);
+			}
+		}, 10);
 
-  const initFromStorage = React.useCallback(async () => {
-    const results = await chrome.storage.local.get([MOCKSI_POPUP_LOCATION]);
-    const storedTransform = results[MOCKSI_POPUP_LOCATION];
-    if (storedTransform) {
-      prevTransform.current = storedTransform;
-      setTransform(storedTransform);
-    }
-  }, []);
+	const initFromStorage = React.useCallback(async () => {
+		const results = await chrome.storage.local.get([MOCKSI_POPUP_LOCATION]);
+		const storedTransform = results[MOCKSI_POPUP_LOCATION];
+		if (storedTransform) {
+			prevTransform.current = storedTransform;
+			setTransform(storedTransform);
+		}
+	}, []);
 
-  React.useEffect(() => {
-    if (initRef.current) {
-      if (dragElRef.current?.style) {
-        //		dragElRef.current.style.opacity = "0";
-      }
-      initFromStorage();
-      window.addEventListener("mouseup", stopDragging);
-      window.addEventListener("mouseleave", stopDragging);
-      initRef.current = false;
-    } else {
-      if (!dragging) {
-        persistTransform();
-      }
-      if (dragElRef.current?.style) {
-        //			dragElRef.current.style.opacity = "1";
-        dragElRef.current.style.transform = `translate(${transform.x}px, ${transform.y}px)`;
-      }
-    }
-    return () => {
-      persistTransform();
-      window.removeEventListener("mouseup", stopDragging);
-      window.removeEventListener("mouseleave", stopDragging);
-    };
-  }, [stopDragging, dragging, persistTransform, transform, initFromStorage]);
+	React.useEffect(() => {
+		if (initRef.current) {
+			if (dragElRef.current?.style) {
+				//		dragElRef.current.style.opacity = "0";
+			}
+			initFromStorage();
+			window.addEventListener("mouseup", stopDragging);
+			window.addEventListener("mouseleave", stopDragging);
+			initRef.current = false;
+		} else {
+			if (!dragging) {
+				persistTransform();
+			}
+			if (dragElRef.current?.style) {
+				//			dragElRef.current.style.opacity = "1";
+				dragElRef.current.style.transform = `translate(${transform.x}px, ${transform.y}px)`;
+			}
+		}
+		return () => {
+			persistTransform();
+			window.removeEventListener("mouseup", stopDragging);
+			window.removeEventListener("mouseleave", stopDragging);
+		};
+	}, [stopDragging, dragging, persistTransform, transform, initFromStorage]);
 
-  return (
-    <div
-      className={`${className ?? ""}`}
-      onMouseDown={() => setDragging(true)}
-      onMouseLeave={stopDragging}
-      onMouseMove={updateTransformOnDrag}
-      onMouseUp={stopDragging}
-      ref={dragElRef}
-      tabIndex={-1}
-    >
-      {children}
-    </div>
-  );
+	return isOpen ? (
+		<div
+			className={`${className ?? ""}`}
+			onMouseDown={() => setDragging(true)}
+			onMouseLeave={stopDragging}
+			onMouseMove={updateTransformOnDrag}
+			onMouseUp={stopDragging}
+			ref={dragElRef}
+			tabIndex={-1}
+		>
+			{children}
+		</div>
+	) : null;
 }
 
 export default Draggable;
