b071a59d9fbae71ae6e4a8f1defab9af9e9e05a2
Brennen Denomme <brennenkdenomme@gmail.com>
2024-08-12 14:25:26 -0400

    iterate entire body when apply alterations (#143)
    * iterate entire body when apply alterations
    
    * biome

--- apps/mocksi-lite/content/EditMode/utils.ts
+++ apps/mocksi-lite/content/EditMode/utils.ts
@@ -17,7 +17,9 @@ export const buildQuerySelector = (
 		keyToSave += `#${id}`;
 	}
 	if (classList.length) {
-		const filteredClasses = [...classList].filter((cls) => !cls.includes(":"));
+		const filteredClasses = [...classList].filter(
+			(cls) => !(cls.includes(":") || cls.includes(".")),
+		);
 		keyToSave += `.${filteredClasses.join(".")}`;
 	}
 	let elements: NodeListOf<Element>;
