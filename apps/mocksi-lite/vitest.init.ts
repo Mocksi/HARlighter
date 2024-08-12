fcd78d88c458f4649147648a4c39cb0e5fb8d5ce
Kayla Fitzsimmons <kayla@Kaylas-MacBook-Pro.local>
2024-08-12 16:30:14 -0700

    cleanup unused test deps

--- apps/mocksi-lite/vitest.init.ts
+++ apps/mocksi-lite/vitest.init.ts
@@ -1,21 +1,18 @@
-import * as matchers from "@testing-library/jest-dom/matchers";
 import "@testing-library/jest-dom/vitest";
-import { cleanup } from "@testing-library/react";
 import { JSDOM } from "jsdom";
-import { afterEach, expect } from "vitest";
 import { vi } from "vitest";
 
 const ChromeMock = {
-	storage: {
-		local: {
-			get: vi.fn().mockImplementation((storage) => {
-				return storage;
-			}),
-			set: vi.fn().mockImplementation((storage) => {
-				return storage;
-			}),
-		},
-	},
+  storage: {
+    local: {
+      get: vi.fn().mockImplementation((storage) => {
+        return storage;
+      }),
+      set: vi.fn().mockImplementation((storage) => {
+        return storage;
+      }),
+    },
+  },
 };
 
 vi.stubGlobal("chrome", ChromeMock);
