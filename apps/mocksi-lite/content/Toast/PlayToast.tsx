c94a368e53cb91a8619d9e4e63ebd50e532e8cea
Kayla Fitzsimmons <kayla@Kaylas-MacBook-Pro.local>
2024-08-14 12:41:55 -0700

    needs format / lint fixes

--- apps/mocksi-lite/content/Toast/PlayToast.tsx
+++ apps/mocksi-lite/content/Toast/PlayToast.tsx
@@ -5,11 +5,11 @@ import { EditIcon, StopIcon } from "../../common/Icons";
 import { Logo } from "../../common/Logos";
 import { MOCKSI_ALTERATIONS, MOCKSI_RECORDING_CREATED_AT } from "../../consts";
 import {
-  getAlterations,
-  loadAlterations,
-  loadPreviousModifications,
-  sendMessage,
-  undoModifications,
+	getAlterations,
+	loadAlterations,
+	loadPreviousModifications,
+	sendMessage,
+	undoModifications,
 } from "../../utils";
 import { AppEvent, AppStateContext } from "../AppStateContext";
 import { getHighlighter } from "../EditMode/highlighter";
@@ -18,82 +18,82 @@ import { observeUrlChange } from "../utils/observeUrlChange";
 import Toast from "./index";
 
 interface PlayToastProps {
-  close: () => void;
+	close: () => void;
 }
 
 const PlayToast = ({ close }: PlayToastProps) => {
-  const { dispatch } = useContext(AppStateContext);
-  const [alterations, setAlterations] = useState<Alteration[]>([]);
-  const [url, setUrl] = useState<string>(document.location.href);
-  const images = useImages();
+	const { dispatch } = useContext(AppStateContext);
+	const [alterations, setAlterations] = useState<Alteration[]>([]);
+	const [url, setUrl] = useState<string>(document.location.href);
+	const images = useImages();
 
-  useEffect(() => {
-    chrome.storage.local
-      .get([MOCKSI_ALTERATIONS, MOCKSI_RECORDING_CREATED_AT])
-      .then((result) => {
-        const alterations = result[MOCKSI_ALTERATIONS];
-        setAlterations(alterations);
+	useEffect(() => {
+		chrome.storage.local
+			.get([MOCKSI_ALTERATIONS, MOCKSI_RECORDING_CREATED_AT])
+			.then((result) => {
+				const alterations = result[MOCKSI_ALTERATIONS];
+				setAlterations(alterations);
 
-        const createdAt = result[MOCKSI_RECORDING_CREATED_AT];
+				const createdAt = result[MOCKSI_RECORDING_CREATED_AT];
 
-        loadAlterations(alterations, {
-          createdAt,
-          withHighlights: false,
-        });
-      });
+				loadAlterations(alterations, {
+					createdAt,
+					withHighlights: false,
+				});
+			});
 
-    const disconnect = observeUrlChange(() => {
-      setUrl(document.location.href);
-    });
+		const disconnect = observeUrlChange(() => {
+			setUrl(document.location.href);
+		});
 
-    return () => {
-      disconnect();
-    };
-  }, []);
+		return () => {
+			disconnect();
+		};
+	}, []);
 
-  // biome-ignore lint/correctness/useExhaustiveDependencies: we dont use the url but want to run this whenever it changes
-  useEffect(() => {
-    getHighlighter().removeHighlightNodes();
-    loadPreviousModifications(alterations);
-    loadAlterations(alterations, { withHighlights: false });
-    console.log(images.edits);
-    images.applyEdits();
-  }, [url]);
+	// biome-ignore lint/correctness/useExhaustiveDependencies: we dont use the url but want to run this whenever it changes
+	useEffect(() => {
+		getHighlighter().removeHighlightNodes();
+		loadPreviousModifications(alterations);
+		loadAlterations(alterations, { withHighlights: false });
+		console.log(images.edits);
+		images.applyEdits();
+	}, [url]);
 
-  const handleEdit = () => {
-    sendMessage("resetIcon");
+	const handleEdit = () => {
+		sendMessage("resetIcon");
 
-    loadPreviousModifications(alterations);
+		loadPreviousModifications(alterations);
 
-    dispatch({ event: AppEvent.START_EDITING });
-  };
+		dispatch({ event: AppEvent.START_EDITING });
+	};
 
-  const handleHideToast = () => {
-    sendMessage("updateToPauseIcon");
-    dispatch({ event: AppEvent.START_PLAYING });
-    close();
-  };
+	const handleHideToast = () => {
+		sendMessage("updateToPauseIcon");
+		dispatch({ event: AppEvent.START_PLAYING });
+		close();
+	};
 
-  const handleStop = () => {
-    sendMessage("resetIcon");
-    undoModifications(alterations);
-    dispatch({ event: AppEvent.STOP_PLAYING });
-  };
+	const handleStop = () => {
+		sendMessage("resetIcon");
+		undoModifications(alterations);
+		dispatch({ event: AppEvent.STOP_PLAYING });
+	};
 
-  return (
-    <Toast className="mw-gap-4 mw-m-2 mw-px-4 mw-py-3">
-      <CloseButton onClick={handleHideToast} />
-      <Logo />
-      <div className="mw-flex mw-gap-2">
-        <Button onClick={handleStop} variant={Variant.icon}>
-          <StopIcon />
-        </Button>
-        <Button onClick={handleEdit} variant={Variant.icon}>
-          <EditIcon />
-        </Button>
-      </div>
-    </Toast>
-  );
+	return (
+		<Toast className="mw-gap-4 mw-m-2 mw-px-4 mw-py-3">
+			<CloseButton onClick={handleHideToast} />
+			<Logo />
+			<div className="mw-flex mw-gap-2">
+				<Button onClick={handleStop} variant={Variant.icon}>
+					<StopIcon />
+				</Button>
+				<Button onClick={handleEdit} variant={Variant.icon}>
+					<EditIcon />
+				</Button>
+			</div>
+		</Toast>
+	);
 };
 
 export default PlayToast;
