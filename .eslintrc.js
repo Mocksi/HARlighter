0a75e4d35d2f32f0445ac5af94ba75ffa88345cf
Kayla Fitzsimmons <kayla@Kaylas-MacBook-Pro.local>
2024-08-26 15:01:36 -0700

    fix eslint errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,16 @@
 // This configuration only applies to the package manager root.
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  ignorePatterns: ["apps/**", "packages/**"],
   extends: ["@repo/eslint-config/library.js"],
+  ignorePatterns: ["apps/**", "packages/**"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: true,
+  },
+  plugins: ["@typescript-eslint"],
+  root: true,
+  rules: {
+    "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+    "no-unused-vars": "off",
   },
 };
