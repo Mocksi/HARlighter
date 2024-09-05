87eb8f99e139f2dfb15908bab6d4ab0775d4f48b
Kayla Fitzsimmons <fitzk@users.noreply.github.com>
2024-09-05 09:43:12 -0700

    fix env based builds, add QA option for running production locally (#173)

--- apps/mocksi-lite-next/vite.config.ts
+++ apps/mocksi-lite-next/vite.config.ts
@@ -1,10 +1,7 @@
 import { ManifestV3Export, crx } from "@crxjs/vite-plugin";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
-import { defineConfig } from "vite";
-
-import devManifest from "./manifest.dev.json";
-import stagingManifest from "./manifest.staging.json";
+import { defineConfig, loadEnv } from "vite";
 import manifest from "./manifest.json";
 import pkg from "./package.json";
 
@@ -14,50 +11,92 @@ const assetsDir = resolve(root, "assets");
 const outDir = resolve(__dirname, "dist");
 const publicDir = resolve(__dirname, "public");
 
-const isDev = process.env.__DEV__ === "true";
+export default defineConfig(({ mode }) => {
+  let isDev = process.env.__DEV__ === "true";
+  // Keys are used to create stable extension ids for unpacked builds, only use for unpublished extension builds
+  const PROD_UNPACKED_KEY =
+    "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA07fIdpHz/cZNpPxNKlUpUDv0HDDfyWfLwVl1lq/YtXVLIjQ9CRGUH8C4CX+MMZSALTE0PZZu1Tclin/hno9cHDm3/Iv9Ijqij6WEs4BDQ5UKy8Q13gmoJr2+4rUPwxeCuI1HLyUSsBSL3GazdQw68/hf3l43uNfQpphb8fxQEhQyeQBi6YzQYf/W7xepENfh3Xlp4etfMS6Xl5OaAqrnUPQmTsJKEJbqs0o5l2EgoRHqvkJHhZ4zeUWZ/GgoqKxy8UahtmywVxDz73zmrBLP3thPaQbiNBRhpKOR/Ldto4P7tm6IdTQgI9CuOCKwZtG9G4j1yXhVX8MfKk17QY4gSwIDAQAB";
+  const DEV_UNPACKED_KEY =
+    "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAtdeTUTUuHZx1xSvDuISF7wJP8nEPwW4vP8zTkdfdtb/1wM0JJ7XFeUHSIiq8l4Cs6/2f8tpK6MspeD7WhwuFWFHA2GWKoLSP0keuuBAhUFKrfISlNwFaNLX5LRkLSZQnr0ujIznvEuRZaXydIYR1e9pdhYTIcp2ToHW4CI02FUBtJVUUVeKGDiKKlKUrxwGtt1ecGZwVrQ1t7dj3DLrKguw1bONtoczFT3cCs9oVYg4l8frzlyI6xfsX8ynd4F+xS6+gYQ3aJBj7phAWHGAxbVRxTAzpzXRkb9A3ne31Ysjy4uYF9x7fK6NvDj/cm+EEfGDb3VmyXvOa3zeerXtdmwIDAQAB";
 
-const extensionENV = process.env.EXTENSION_ENV || "development";
-let activeManifest;
+  const modes = ["QA", "staging", "production", "development"];
+  if (!mode || !modes.includes(mode)) {
+    mode = "production";
+  }
 
-switch (extensionENV) {
-  case "staging":
-    activeManifest = Object.assign(manifest, stagingManifest);
-    break;
-  case "production":
-    activeManifest = manifest;
-    break;
-  default:
-    activeManifest = Object.assign(manifest, devManifest);
-    break;
-}
+  const envMode = mode === "QA" ? "production" : mode;
+  const envFileName = envMode === "production" ? ".env" : `.env.${envMode}`;
 
-const extensionManifest = {
-  ...activeManifest,
-  name: activeManifest.name,
-  version: pkg.version,
-};
+  const env = loadEnv(envMode, process.cwd());
 
-export default defineConfig({
-  build: {
-    emptyOutDir: !isDev,
-    outDir,
-    sourcemap: isDev,
-  },
-  plugins: [
-    react(),
-    crx({
-      contentScripts: {
-        injectCss: true,
-      },
-      manifest: extensionManifest as ManifestV3Export,
-    }),
-  ],
-  publicDir,
-  resolve: {
-    alias: {
-      "@assets": assetsDir,
-      "@pages": pagesDir,
-      "@src": root,
+  if (!env.VITE_NEST_APP || !env.VITE_NEST_APP.includes("http")) {
+    throw new Error(
+      `VITE_NEST_APP is not set correctly, please check .env files: ${env.VITE_NEST_APP}`,
+    );
+  }
+
+  // Dev note: make sure these urls are what you expect them to be for current mode!
+  manifest.externally_connectable.matches = [`${env.VITE_NEST_APP}/*`];
+  manifest.content_security_policy.extension_pages = `object-src 'none'; child-src ${env.VITE_NEST_APP}; frame-src ${env.VITE_NEST_APP}; script-src 'self'`;
+
+  // If mode is 'production' apply manifest as is, note, published prod extension
+  // manifest should not include 'key'
+  switch (mode) {
+    // QA creates a production build that can be run locally
+    case "QA":
+      manifest.name = "[QA Production] Mocksi Lite";
+      manifest.key = PROD_UNPACKED_KEY;
+      break;
+    case "development":
+      manifest.name = "[Development] Mocksi Lite";
+      manifest.key = DEV_UNPACKED_KEY;
+      break;
+    case "staging":
+      manifest.name = "[Staging] Mocksi Lite";
+      manifest.key = DEV_UNPACKED_KEY;
+      break;
+  }
+  console.log("** BUILDING MOCKSI LITE EXTENSION **");
+  console.log(`TARGET:   "${mode}"`);
+  console.log(`ENV FILE: "${envFileName}"`);
+  console.log(`NEST URL: "${env.VITE_NEST_APP}"`);
+  console.log(`MANIFEST: "${manifest.name}"`);
+  console.log(" ");
+
+  if (mode === "production" && manifest.key) {
+    throw new Error(
+      'Production manifest should not have a "key", please remove',
+    );
+  }
+
+  const extensionManifest = {
+    ...manifest,
+    name: manifest.name,
+    version: pkg.version,
+  };
+
+  return {
+    build: {
+      emptyOutDir: !isDev,
+      outDir,
+      sourcemap: isDev,
     },
-  },
+    plugins: [
+      react(),
+      crx({
+        contentScripts: {
+          injectCss: true,
+        },
+        manifest: extensionManifest as ManifestV3Export,
+      }),
+    ],
+    publicDir,
+    resolve: {
+      alias: {
+        "@assets": assetsDir,
+        "@pages": pagesDir,
+        "@src": root,
+      },
+    },
+  };
 });
