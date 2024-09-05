5e990e68891ea4e88f1264ab64a794fa21b30526
Kayla Fitzsimmons <kayla@Kaylas-MacBook-Pro.local>
2024-09-05 01:27:45 -0700

    make it harder to mess up our config

--- apps/mocksi-lite-next/vite.config.ts
+++ apps/mocksi-lite-next/vite.config.ts
@@ -1,7 +1,7 @@
 import { ManifestV3Export, crx } from "@crxjs/vite-plugin";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import manifest from "./manifest.json";
 import pkg from "./package.json";
 
@@ -11,10 +11,10 @@ const assetsDir = resolve(root, "assets");
 const outDir = resolve(__dirname, "dist");
 const publicDir = resolve(__dirname, "public");
 
-export default defineConfig(async ({ mode }) => {
+export default defineConfig(({ mode }) => {
   let isDev = process.env.__DEV__ === "true";
-  // Keys are used to create stable extension ids for unpacked builds
-  const QA_UNPACKED_KEY =
+  // Keys are used to create stable extension ids for unpacked builds, only use for unpublished extension builds
+  const PROD_UNPACKED_KEY =
     "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA07fIdpHz/cZNpPxNKlUpUDv0HDDfyWfLwVl1lq/YtXVLIjQ9CRGUH8C4CX+MMZSALTE0PZZu1Tclin/hno9cHDm3/Iv9Ijqij6WEs4BDQ5UKy8Q13gmoJr2+4rUPwxeCuI1HLyUSsBSL3GazdQw68/hf3l43uNfQpphb8fxQEhQyeQBi6YzQYf/W7xepENfh3Xlp4etfMS6Xl5OaAqrnUPQmTsJKEJbqs0o5l2EgoRHqvkJHhZ4zeUWZ/GgoqKxy8UahtmywVxDz73zmrBLP3thPaQbiNBRhpKOR/Ldto4P7tm6IdTQgI9CuOCKwZtG9G4j1yXhVX8MfKk17QY4gSwIDAQAB";
   const DEV_UNPACKED_KEY =
     "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAtdeTUTUuHZx1xSvDuISF7wJP8nEPwW4vP8zTkdfdtb/1wM0JJ7XFeUHSIiq8l4Cs6/2f8tpK6MspeD7WhwuFWFHA2GWKoLSP0keuuBAhUFKrfISlNwFaNLX5LRkLSZQnr0ujIznvEuRZaXydIYR1e9pdhYTIcp2ToHW4CI02FUBtJVUUVeKGDiKKlKUrxwGtt1ecGZwVrQ1t7dj3DLrKguw1bONtoczFT3cCs9oVYg4l8frzlyI6xfsX8ynd4F+xS6+gYQ3aJBj7phAWHGAxbVRxTAzpzXRkb9A3ne31Ysjy4uYF9x7fK6NvDj/cm+EEfGDb3VmyXvOa3zeerXtdmwIDAQAB";
@@ -24,31 +24,49 @@ export default defineConfig(async ({ mode }) => {
     mode = "production";
   }
 
-  console.log(`Build target: ${mode}`);
+  const envMode = mode === "QA" ? "production" : mode;
+  const envFileName = envMode === "production" ? ".env" : `.env.${envMode}`;
+
+  const env = loadEnv(envMode, process.cwd());
+
+  if (!env.VITE_NEST_APP || !env.VITE_NEST_APP.includes("http")) {
+    throw new Error(
+      `VITE_NEST_APP is not set correctly, please check .env files: ${env.VITE_NEST_APP}`,
+    );
+  }
+
+  // Dev note: make sure these urls are what you expect them to be for current mode!
+  manifest.externally_connectable.matches = [`${env.VITE_NEST_APP}/*`];
+  manifest.content_security_policy.extension_pages = `object-src 'none'; child-src ${env.VITE_NEST_APP} frame-src ${env.VITE_NEST_APP}; script-src 'self'`;
+
   // If mode is 'production' apply manifest as is, note, published prod extension
   // manifest should not include 'key'
   switch (mode) {
     // QA creates a production build that can be run locally
     case "QA":
       manifest.name = "[QA Production] Mocksi Lite";
-      manifest.key = QA_UNPACKED_KEY;
+      manifest.key = PROD_UNPACKED_KEY;
       break;
     case "development":
       manifest.name = "[Development] Mocksi Lite";
-      manifest.externally_connectable.matches = ["http://localhost:3030/*"];
-      manifest.content_security_policy.extension_pages =
-        "object-src 'none'; child-src http://localhost:3030; frame-src http://localhost:3030; script-src 'self'";
       manifest.key = DEV_UNPACKED_KEY;
       break;
     case "staging":
       manifest.name = "[Staging] Mocksi Lite";
-      manifest.externally_connectable.matches = [
-        "https://nest-auth-ts-merge.onrender.com/*",
-      ];
-      manifest.content_security_policy.extension_pages =
-        "object-src 'none'; child-src https://nest-auth-ts-merge.onrender.com; frame-src https://nest-auth-ts-merge.onrender.com; script-src 'self'";
       manifest.key = DEV_UNPACKED_KEY;
       break;
+  }
+  console.log("** BUILDING MOCKSI LITE EXTENSION **");
+  console.log(`TARGET:   "${mode}"`);
+  console.log(`ENV FILE: "${envFileName}"`);
+  console.log(`NEST URL: "${env.VITE_NEST_APP}"`);
+  console.log(`MANIFEST: "${manifest.name}"`);
+  console.log(" ");
+
+  if (mode === "production" && manifest.key) {
+    throw new Error(
+      'Production manifest should not have a "key", please remove',
+    );
   }
 
   const extensionManifest = {
