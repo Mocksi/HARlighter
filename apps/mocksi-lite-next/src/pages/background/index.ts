52be27eac19c6f22768376c59b626f0e8a1f4f71
Kayla Fitzsimmons <fitzk@users.noreply.github.com>
2024-09-20 10:07:01 -0700

    adds top toolbar, including undo (#187)
    * adds top toolbar
    
    * remove background change
    
    * add long lived connection for top -> main nest component comms
    
    * improve source value
    
    * add undo doesnt handle empty edits / edge cases
    
    * add message to move reactor back into mocksi-extension.tsx
    
    * make sure top iframe knows when # edits updates
    
    * cleanup logging, make sure edits are passed to extension/top
    
    * remove unused listener
    
    * coderrabbit PR feedback
    
    * save merge conflict resolved
    
    ---------
    
    Co-authored-by: Kayla Fitzsimmons <kayla@Kaylas-MacBook-Pro.local>
    Co-authored-by: Kayla Fitzsimmons <kayla@Kaylas-MBP.jetpack>

--- apps/mocksi-lite-next/src/pages/background/index.ts
+++ apps/mocksi-lite-next/src/pages/background/index.ts
@@ -1,4 +1,9 @@
-import { AppEvents, AuthEvents, LayoutEvents } from "@pages/events";
+import {
+  AppEvents,
+  AuthEvents,
+  DemoEditEvents,
+  LayoutEvents,
+} from "@pages/events";
 import { jwtDecode } from "jwt-decode";
 
 console.log("background script loaded");
@@ -109,6 +114,19 @@ addEventListener("install", () => {
   });
 });
 
+let mainIframeSrcPort: null | chrome.runtime.Port = null;
+let topIframeSrcPort: null | chrome.runtime.Port = null;
+
+chrome.runtime.onConnectExternal.addListener((port) => {
+  console.log("connecting...", port);
+  if (port.name === "extension/main") {
+    mainIframeSrcPort = port;
+  }
+  if (port.name === "extension/top") {
+    topIframeSrcPort = port;
+  }
+});
+
 // when user clicks toolbar mount extension
 chrome.action.onClicked.addListener((tab) => {
   if (!tab?.id) {
@@ -131,17 +149,6 @@ chrome.action.onClicked.addListener((tab) => {
   }
 });
 
-chrome.runtime.onMessage.addListener(
-  (request, _sender, sendResponse): boolean => {
-    sendResponse({
-      data: request.data,
-      message: request.message,
-      status: "ok",
-    });
-    return true;
-  },
-);
-
 chrome.runtime.onMessageExternal.addListener(
   (request, _sender, sendResponse) => {
     console.log("on message external: ", request);
@@ -149,10 +156,26 @@ chrome.runtime.onMessageExternal.addListener(
     // execute in async block so that we return true
     // synchronously, telling chrome to wait for the response
     (async () => {
+      if (
+        request.source === "extension/top" &&
+        request.message === AppEvents.EDIT_DEMO_STOP
+      ) {
+        if (mainIframeSrcPort) {
+          // notify extension/main that demo edit mode exited in extension/top
+          mainIframeSrcPort.postMessage({
+            ...request,
+            message: AppEvents.EDIT_DEMO_STOP,
+          });
+        } else {
+          console.log("mainIframeSrcPort is not connected");
+        }
+      }
+
       if (request.message === AuthEvents.AUTH_ERROR) {
         await clearAuth();
         sendResponse({
           message: AuthEvents.RETRY,
+          source: "background",
           status: "ok",
         });
       } else if (request.message === AuthEvents.UNAUTHORIZED) {
@@ -162,12 +185,14 @@ chrome.runtime.onMessageExternal.addListener(
           const tab = await getCurrentTab();
           sendResponse({
             message: { accessToken, email, url: tab?.url },
+            source: "background",
             status: "ok",
           });
         } else {
           await showAuthTab(true);
           sendResponse({
             message: AuthEvents.AUTHENTICATING,
+            source: "background",
             status: "ok",
           });
         }
@@ -176,9 +201,11 @@ chrome.runtime.onMessageExternal.addListener(
         if (!tab?.id) {
           sendResponse({
             message: LayoutEvents.NO_TAB,
+            source: "background",
             status: "ok",
           });
-          return;
+          console.error("No tab found");
+          return true;
         }
 
         if (
@@ -201,16 +228,39 @@ chrome.runtime.onMessageExternal.addListener(
           showDefaultIcon(tab.id);
         }
 
-        chrome.tabs.sendMessage(
-          tab.id,
-          {
-            data: request.data,
-            message: request.message,
-          },
-          (response) => {
-            sendResponse(response);
-          },
-        );
+        // send message to iframes and reactor in mocksi-extension
+        chrome.tabs.sendMessage(tab.id, request, async (response) => {
+          console.log("response from content script in background:", response);
+          if (response.message === DemoEditEvents.UNDO) {
+            // pass updated modifications from reactor to extension/main to store
+            if (mainIframeSrcPort) {
+              await mainIframeSrcPort.postMessage({
+                ...response,
+                status: "ok", // response handler expects status
+              });
+            } else {
+              console.log("mainIframeSrcPort is not connected");
+            }
+          }
+          if (
+            request.message === AppEvents.EDIT_DEMO_START ||
+            request.message === DemoEditEvents.NEW_EDIT ||
+            request.message === DemoEditEvents.CHAT_RESPONSE
+          ) {
+            // notify extension/top # of edits changed
+            if (topIframeSrcPort) {
+              await topIframeSrcPort.postMessage({
+                ...response,
+                status: "ok",
+              });
+            } else {
+              console.log("topIframeSrcPort is not connected");
+            }
+          }
+
+          sendResponse(response);
+        });
+        return true;
       }
     })();
 
--- apps/mocksi-lite-next/src/pages/content/mocksi-extension.tsx
+++ apps/mocksi-lite-next/src/pages/content/mocksi-extension.tsx
@@ -1,25 +1,11 @@
 import type { ModificationRequest } from "@repo/reactor";
 import { Reactor } from "@repo/reactor";
 import React from "react";
-import ReactDOM from "react-dom";
 import { createRoot } from "react-dom/client";
-import { DemoEditEvents, LayoutEvents } from "../events";
+import { AppEvents, DemoEditEvents, LayoutEvents } from "../events";
 import { getHighlighter } from "./highlighter";
-
-export enum IframePosition {
-  BOTTOM_CENTER = "BOTTOM_CENTER",
-  TOP_RIGHT = "TOP_RIGHT",
-  BOTTOM_RIGHT = "BOTTOM_RIGHT",
-}
-export interface IframeResizeArgs {
-  height: number;
-  id?: string;
-  position:
-    | IframePosition.BOTTOM_CENTER
-    | IframePosition.BOTTOM_RIGHT
-    | IframePosition.TOP_RIGHT;
-  width: number;
-}
+import MainIframe from "./main-iframe";
+import TopIframe from "./top-iframe";
 
 const STORAGE_CHANGE_EVENT = "MOCKSI_STORAGE_CHANGE";
 
@@ -28,8 +14,6 @@ div.id = "__mocksi__root";
 document.body.appendChild(div);
 
 let mounted = false;
-const reactor = new Reactor();
-const highlighter = getHighlighter();
 
 window.addEventListener("message", (event: MessageEvent) => {
   const eventData = event.data;
@@ -45,56 +29,6 @@ window.addEventListener("message", (event: MessageEvent) => {
   }
 });
 
-function getIframeStyles({ height, position, width }: IframeResizeArgs) {
-  if (!height || !width || !position) {
-    console.error(
-      "Cannot update iframe size / position, make sure 'request.data.iframe' has 'height', 'width', and 'position' set correctly",
-    );
-    return;
-  }
-
-  const bounds = document.body.getBoundingClientRect();
-
-  let styles = {};
-  switch (position) {
-    case IframePosition.BOTTOM_CENTER:
-      styles = {
-        bottom: "0px",
-        right: `${bounds.width / 2 - width / 2}px`,
-        top: "auto",
-      };
-      break;
-    case IframePosition.BOTTOM_RIGHT:
-      styles = {
-        bottom: "10px",
-        right: "10px",
-        top: "auto",
-      };
-      break;
-    case IframePosition.TOP_RIGHT:
-      styles = {
-        bottom: "auto",
-        display: "block",
-        right: "10px",
-        top: "10px",
-      };
-      break;
-  }
-
-  return Object.assign(
-    {
-      bottom: "auto",
-      display: "block",
-      height: `${height}px`,
-      left: "auto",
-      right: "auto",
-      top: "auto",
-      width: `${width}px`,
-    },
-    styles,
-  );
-}
-
 interface AppMessageRequest {
   data: {
     edits?: ModificationRequest[];
@@ -103,190 +37,170 @@ interface AppMessageRequest {
   message: string;
 }
 
-async function handleStartStopDemoEvent(
-  prevRequest: AppMessageRequest,
-  request: AppMessageRequest,
-) {
-  async function startDemo(request: AppMessageRequest) {
-    if (request.data.edits?.length) {
-      for (const mod of request.data.edits) {
-        await reactor.pushModification(mod);
-      }
-    } else {
-      console.log("no edits provided to reactor");
-    }
-    return await reactor.attach(document, highlighter);
-  }
+function Extension() {
+  const prevStartStopDemoEventRef = React.useRef({
+    data: { uuid: "" },
+    message: "",
+  });
 
-  // check if app is asking to start or stop PLAY or EDIT
-  const startRequestRegExp = new RegExp(/_DEMO_START/);
-  const stopRequestRegExp = new RegExp(/_DEMO_STOP/);
+  const reactor = new Reactor();
+  const highlighter = getHighlighter();
 
-  const requestingStopDemo = stopRequestRegExp.test(request.message);
-  const requestingStartDemo = startRequestRegExp.test(request.message);
-
-  if (!requestingStartDemo && !requestingStopDemo) {
-    return prevRequest;
-  }
-  // if a demo is running already we want to avoid mounting the same
-  // modifications more than once, this is more performant, and edits
-  // persist in the dom if transitioning between EDIT and PLAY states
-  if (requestingStartDemo) {
-    const prevDemoUUID = prevRequest?.data?.uuid || null;
-
-    const demoRunning =
-      prevDemoUUID && startRequestRegExp.test(prevRequest.message);
-
-    if (!demoRunning) {
-      await startDemo(request);
-    } else {
-      const isDupeEvent = prevRequest.message === request.message;
-      const isNewDemo = prevDemoUUID !== request.data.uuid;
-
-      if (!isDupeEvent && isNewDemo) {
-        if (reactor.isAttached()) {
-          await reactor.detach(true);
+  async function handleStartStopDemoEvent(
+    prevRequest: AppMessageRequest,
+    request: AppMessageRequest,
+  ) {
+    async function startDemo(request: AppMessageRequest) {
+      if (request.data.edits?.length) {
+        for (const mod of request.data.edits) {
+          await reactor.pushModification(mod);
         }
+      } else {
+        console.debug("no edits provided to reactor");
+      }
+      return await reactor.attach(document, highlighter);
+    }
+
+    // check if app is asking to start or stop PLAY or EDIT
+    const startRequestRegExp = new RegExp(/_DEMO_START/);
+    const stopRequestRegExp = new RegExp(/_DEMO_STOP/);
+
+    const requestingStopDemo = stopRequestRegExp.test(request.message);
+    const requestingStartDemo = startRequestRegExp.test(request.message);
+
+    if (!requestingStartDemo && !requestingStopDemo) {
+      return prevRequest;
+    }
+
+    // if a demo is running already we want to avoid mounting the same
+    // modifications more than once, this is more performant, and edits
+    // persist in the dom if transitioning between EDIT and PLAY states
+    if (requestingStartDemo) {
+      const prevDemoUUID = prevRequest?.data?.uuid || null;
+
+      const demoRunning =
+        prevDemoUUID && startRequestRegExp.test(prevRequest.message);
+
+      if (!demoRunning) {
         await startDemo(request);
+      } else {
+        const isDupeEvent = prevRequest.message === request.message;
+        const isNewDemo = prevDemoUUID !== request.data.uuid;
+        if (!isDupeEvent && isNewDemo) {
+          if (reactor.isAttached()) {
+            await reactor.detach(true);
+          }
+          await startDemo(request);
+        }
       }
     }
+
+    if (requestingStopDemo) {
+      await reactor.detach(true);
+    }
+
+    return request;
   }
 
-  if (requestingStopDemo) {
-    await reactor.detach(true);
+  async function findReplaceAll(find: string, replace: string, flags: string) {
+    const modification: ModificationRequest = {
+      description: `Change ${find} to ${replace}`,
+      modifications: [
+        {
+          action: "replaceAll",
+          content: `/${find}/${replace}/${flags}`,
+          selector: "body",
+        },
+      ],
+    };
+
+    const modifications = await reactor.pushModification(modification);
+    return modifications;
   }
 
-  return request;
+  React.useEffect(() => {
+    chrome.runtime.onMessage.addListener((request, _sender, sendResponse) => {
+      // execute in async block so that we return true
+      // synchronously, telling chrome to wait for the response
+      (async () => {
+        let data = null;
+        console.debug("request in mocksi-extension from background: ", request);
+        prevStartStopDemoEventRef.current = await handleStartStopDemoEvent(
+          prevStartStopDemoEventRef.current,
+          request,
+        );
+
+        // make sure edits are available to extension/top
+        if (request.message === AppEvents.EDIT_DEMO_START) {
+          data = {
+            edits: Array.from(reactor.getAppliedModifications()).map(
+              (mod) => mod.modificationRequest,
+            ),
+            uuid: request.data.uuid,
+          };
+        }
+
+        if (request.message === DemoEditEvents.NEW_EDIT) {
+          if (request.data) {
+            const { find, flags, replace } = request.data;
+            await findReplaceAll(find, replace, flags);
+            data = Array.from(reactor.getAppliedModifications()).map(
+              (mod) => mod.modificationRequest,
+            );
+          }
+        }
+
+        // chat events
+        if (request.message === DemoEditEvents.CHAT_MESSAGE) {
+          data = reactor.exportDOM();
+        }
+
+        if (request.message === DemoEditEvents.CHAT_RESPONSE) {
+          await reactor.pushModification(request.data);
+          data = Array.from(reactor.getAppliedModifications()).map(
+            (mod) => mod.modificationRequest,
+          );
+        }
+
+        if (request.message === DemoEditEvents.UNDO) {
+          await reactor.popModification();
+          data = Array.from(reactor.getAppliedModifications()).map(
+            (mod) => mod.modificationRequest,
+          );
+        }
+
+        // send response back to background which forwards it to extension/x
+        sendResponse({
+          data,
+          message: request.message,
+          source: "reactor",
+          status: "ok",
+        });
+      })();
+      return true;
+    });
+  }, []);
+  return (
+    <>
+      <TopIframe />
+      <MainIframe />
+    </>
+  );
 }
 
 chrome.runtime.onMessage.addListener((request) => {
   if (request.message === LayoutEvents.MOUNT) {
     const rootContainer = document.querySelector("#__mocksi__root");
-    if (!rootContainer) throw new Error("Can't find Content root element");
+    if (!rootContainer) {
+      throw new Error("Can't find Content root element");
+    }
 
     const root = createRoot(rootContainer);
-    const Iframe = () => {
-      const prevStartStopDemoEventRef = React.useRef({
-        data: { uuid: "" },
-        message: "",
-      });
-      const iframeRef = React.useRef<HTMLIFrameElement>(null);
-
-      async function findReplaceAll(
-        find: string,
-        replace: string,
-        flags: string,
-      ) {
-        const modification: ModificationRequest = {
-          description: `Change ${find} to ${replace}`,
-          modifications: [
-            {
-              action: "replaceAll",
-              content: `/${find}/${replace}/${flags}`,
-              selector: "body",
-            },
-          ],
-        };
-
-        const modifications = await reactor.pushModification(modification);
-        return modifications;
-      }
-
-      React.useEffect(() => {
-        chrome.runtime.onMessage.addListener(
-          (request, _sender, sendResponse) => {
-            // execute in async block so that we return true
-            // synchronously, telling chrome to wait for the response
-            (async () => {
-              let data = null;
-
-              prevStartStopDemoEventRef.current =
-                await handleStartStopDemoEvent(
-                  prevStartStopDemoEventRef.current,
-                  request,
-                );
-
-              if (request.message === DemoEditEvents.NEW_EDIT) {
-                if (request.data) {
-                  const { find, flags, replace } = request.data;
-                  await findReplaceAll(find, replace, flags);
-                  data = Array.from(reactor.getAppliedModifications()).map(
-                    (mod) => mod.modificationRequest,
-                  );
-                }
-              }
-              // chat events
-              if (request.message === DemoEditEvents.CHAT_MESSAGE) {
-                data = reactor.exportDOM();
-              }
-              if (request.message === DemoEditEvents.CHAT_RESPONSE) {
-                await reactor.pushModification(request.data);
-                data = Array.from(reactor.getAppliedModifications()).map(
-                  (mod) => mod.modificationRequest,
-                );
-              }
-
-              // Resize iframe, how or hide it
-              if (iframeRef.current) {
-                switch (request.message) {
-                  case LayoutEvents.HIDE:
-                    iframeRef.current.style.display = "none";
-                    break;
-                  case LayoutEvents.SHOW:
-                    iframeRef.current.style.display = "block";
-                    break;
-                  case LayoutEvents.RESIZE:
-                    const styles = getIframeStyles(request.data.iframe);
-                    Object.assign(iframeRef.current.style, styles);
-                }
-              }
-
-              sendResponse({
-                data,
-                message: request.message,
-                status: "ok",
-              });
-            })();
-            return true;
-          },
-        );
-      }, []);
-
-      return (
-        <div>
-          {ReactDOM.createPortal(
-            <>
-              <iframe
-                ref={iframeRef}
-                seamless={true}
-                src={`${import.meta.env.VITE_NEST_APP}/extension`}
-                style={{
-                  backgroundColor: "transparent",
-                  border: "none",
-                  bottom: "10px",
-                  boxShadow: "none",
-                  colorScheme: "light",
-                  display: "block",
-                  height: "600px",
-                  left: "auto",
-                  position: "fixed",
-                  right: "10px",
-                  top: "auto",
-                  width: "500px",
-                  zIndex: 99998,
-                }}
-              />
-            </>,
-            document.body,
-          )}
-        </div>
-      );
-    };
 
     // avoid remounting react tree
     try {
       if (!mounted) {
-        root.render(<Iframe />);
+        root.render(<Extension />);
         mounted = true;
       }
       console.debug("content script loaded, extension iframe mounted");
